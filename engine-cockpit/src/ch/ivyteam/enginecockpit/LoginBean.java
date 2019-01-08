cdb0557be90c9101ce6e9a62ecaf047d346e762d
Lukas Lieb <lukas.lieb@axonivy.com>
2019-01-08 13:40:12 +0100

    XIVY-3027 add login and logout function

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/LoginBean.java
@@ -0,0 +1,62 @@
+package ch.ivyteam.enginecockpit;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import ch.ivyteam.ivy.security.ISession;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	private String userName;
+	private String password;
+	
+	public void checkLogin() {
+		if (ISession.get().isSessionUserUnknown()) {
+			redirect("Login.xhtml");
+		}
+	}
+
+	public void login() {
+		if (ISession.get().loginSessionUser(userName, password)) {
+			redirect("Dashboard.xhtml");
+			return;
+		} 
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login failed", "Login failed"));
+	}
+	
+	public void logout() {
+		throw new RuntimeException("test");
+//		ISession.get().logoutSessionUser();
+//		redirect("Login.xhtml");
+	}
+	
+	private void redirect(String url) {
+		try {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().redirect(url);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not send redirect", e);
+		}
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
