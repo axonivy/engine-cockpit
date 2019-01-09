324bc83ceab508722cd45b088642c34db49a2e7f
Lukas Lieb <lukas.lieb@axonivy.com>
2019-01-09 09:32:22 +0100

    XIVY-3027 map propertylist to configproperty objects + short source

--- engine-cockpit/src/ch/ivyteam/enginecockpit/AdvancedConfigBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/AdvancedConfigBean.java
@@ -1,6 +1,7 @@
 package ch.ivyteam.enginecockpit;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -9,43 +10,45 @@ import javax.faces.context.FacesContext;
 
 import org.primefaces.event.SelectEvent;
 
+import ch.ivyteam.enginecockpit.model.ConfigProperty;
 import ch.ivyteam.ivy.configuration.restricted.IConfiguration;
-import ch.ivyteam.ivy.configuration.restricted.Property;
 
 @ManagedBean
 @ViewScoped
 @SuppressWarnings("restriction")
 public class AdvancedConfigBean {
-	private List<Property> configs;
-	private List<Property> filteredConfigs;
-	private Property selectedConfig;
-	
+	private List<ConfigProperty> configs;
+	private List<ConfigProperty> filteredConfigs;
+	private ConfigProperty selectedConfig;
+
 	public AdvancedConfigBean() {
-		configs = IConfiguration.get().getProperties();
+		configs = IConfiguration.get().getProperties().stream()
+				.map(property -> new ConfigProperty(property))
+				.collect(Collectors.toList());
 	}
-	
-	public List<Property> getConfigs() {
+
+	public List<ConfigProperty> getConfigs() {
 		return configs;
 	}
-	
-	public Property getSelectedConfig() {
-        return selectedConfig;
-    }
- 
-    public void setSelectedConfig(Property selectedConfig) {
-        this.selectedConfig = selectedConfig;
-    }
-    
-    public List<Property> getFilteredConfigs() {
-        return filteredConfigs;
-    }
-    
-    public void setFilteredConfigs(List<Property> filteredConfigs) {
-        this.filteredConfigs = filteredConfigs;
-    }
-    
-    public void onRowSelect(SelectEvent event) {
-        FacesMessage msg = new FacesMessage("Config Selected", ((Property) event.getObject()).getKey());
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
+
+	public ConfigProperty getSelectedConfig() {
+		return selectedConfig;
+	}
+
+	public void setSelectedConfig(ConfigProperty selectedConfig) {
+		this.selectedConfig = selectedConfig;
+	}
+
+	public List<ConfigProperty> getFilteredConfigs() {
+		return filteredConfigs;
+	}
+
+	public void setFilteredConfigs(List<ConfigProperty> filteredConfigs) {
+		this.filteredConfigs = filteredConfigs;
+	}
+
+	public void onRowSelect(SelectEvent event) {
+		FacesMessage msg = new FacesMessage("Config Selected", ((ConfigProperty) event.getObject()).getKey());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 }
---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/model/ConfigProperty.java
@@ -0,0 +1,56 @@
+package ch.ivyteam.enginecockpit.model;
+
+import java.io.File;
+
+import ch.ivyteam.ivy.configuration.restricted.Property;
+
+@SuppressWarnings("restriction")
+public class ConfigProperty {
+	private String key;
+	private String value;
+	private String source;
+	private boolean password;
+	
+	public ConfigProperty(Property property) {
+		this.key = property.getKey();
+		this.value = property.getValue();
+		this.source = property.getMetaData().getSource();
+		this.password = property.getMetaData().isPassword();
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getSource() {
+		return source;
+	}
+	
+	public String getShortSource() {
+		return source.substring(source.lastIndexOf(File.separator) + 1);
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public boolean isPassword() {
+		return password;
+	}
+
+	public void setPassword(boolean password) {
+		this.password = password;
+	}
+}
