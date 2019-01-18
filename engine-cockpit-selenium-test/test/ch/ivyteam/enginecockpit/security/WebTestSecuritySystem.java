79fa98da1d6a6a97d8b9f3544acce6be7fe47dd7
Lukas Lieb <lukas.lieb@axonivy.com>
2019-01-18 09:00:48 +0100

    XIVY-3025 add application switch util and test it

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestSecuritySystem.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestSecuritySystem.java
@@ -1,10 +1,11 @@
-package ch.ivyteam.enginecockpit;
+package ch.ivyteam.enginecockpit.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import ch.ivyteam.enginecockpit.WebTestBase;
 import ch.ivyteam.enginecockpit.util.Navigation;
 
 public class WebTestSecuritySystem extends WebTestBase
