f219a1f8a9fb4a8dbd5b8bedbac6355eded17ca3
Lukas Lieb <lukas.lieb@axonivy.com>
2019-01-31 08:17:04 +0100

    XIVY-3028 fix email test on engine

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestEmail.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestEmail.java
@@ -6,9 +6,12 @@ import static org.awaitility.Awaitility.await;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.axonivy.ivy.supplements.primeui.tester.PrimeUi;
@@ -23,28 +26,27 @@ public class WebTestEmail extends WebTestBase
   @Test
   void testEmailLanguageSwitch(FirefoxDriver driver)
   {
-    //TODO: test with language name not with position
     toEmail(driver);
     
     String langDropDownId = driver.findElementByXPath("//div[contains(@id, 'applicationTabView:0:emailSetting:languageDropDown')]").getAttribute("id");
     String saveEmailSettingsBtnId = driver.findElementByXPath("//button[contains(@id, 'applicationTabView:0:saveEmailSettings')]").getAttribute("id");
     driver.findElementById(langDropDownId + "_label").click();
     await().until(() -> driver.findElementById(langDropDownId + "_items").isDisplayed());
-    String chooseLanguage = driver.findElementById(langDropDownId + "_0").getText();
+    Map<String, String> languages = driver.findElementsByXPath("//*[@id='"+ langDropDownId +"_items']/li").stream().collect(Collectors.toMap(WebElement::getText, e -> e.getAttribute("id")));
     saveScreenshot(driver, "languages");
-    driver.findElementById(langDropDownId + "_0").click();
-    await().untilAsserted(() -> assertThat(driver.findElementById(langDropDownId + "_label").getText()).isEqualTo(chooseLanguage));
+    driver.findElementById(languages.get("German")).click();
+    await().untilAsserted(() -> assertThat(driver.findElementById(langDropDownId + "_label").getText()).isEqualTo("German"));
     saveScreenshot(driver, "choose_language");
     driver.findElementById(saveEmailSettingsBtnId).click();
     await().untilAsserted(() -> assertThat(driver.findElementById("form:emailSaveSuccess_container").isDisplayed()).isTrue());
     await().untilAsserted(() -> assertThat(driver.findElementById("form:emailSaveSuccess_container").getText()).isEqualTo("User email changes saved"));
     driver.navigate().refresh();
     saveScreenshot(driver, "refresh");
-    await().untilAsserted(() -> assertThat(driver.findElementById(langDropDownId + "_label").getText()).isEqualTo(chooseLanguage));
+    await().untilAsserted(() -> assertThat(driver.findElementById(langDropDownId + "_label").getText()).isEqualTo("German"));
     
     driver.findElementById(langDropDownId + "_label").click();
     await().until(() -> driver.findElementById(langDropDownId + "_items").isDisplayed());
-    driver.findElementById(langDropDownId + "_1").click();
+    driver.findElementById(languages.get("English")).click();
     await().untilAsserted(() -> assertThat(driver.findElementById(langDropDownId + "_label").getText()).isEqualTo("English"));
     driver.findElementById(saveEmailSettingsBtnId).click();
   }
