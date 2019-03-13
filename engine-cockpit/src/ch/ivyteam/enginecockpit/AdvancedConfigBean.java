4a2959908f1c4e3c7ebc9c78d9ecc151ac90e319
Lukas Lieb <lukas.lieb@axonivy.com>
2019-03-13 10:10:27 +0100

    XIVY-3026 make cockpit release ready
    - remove selection in advanced config
    - mark placeholder panels in dashboard
    - remove buttons without any function yet

--- engine-cockpit/src/ch/ivyteam/enginecockpit/AdvancedConfigBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/AdvancedConfigBean.java
@@ -3,12 +3,8 @@ package ch.ivyteam.enginecockpit;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-
-import org.primefaces.event.SelectEvent;
 
 import ch.ivyteam.enginecockpit.model.ConfigProperty;
 import ch.ivyteam.ivy.configuration.restricted.IConfiguration;
@@ -20,7 +16,6 @@ public class AdvancedConfigBean
 {
   private List<ConfigProperty> configs;
   private List<ConfigProperty> filteredConfigs;
-  private ConfigProperty selectedConfig;
 
   public AdvancedConfigBean()
   {
@@ -34,16 +29,6 @@ public class AdvancedConfigBean
     return configs;
   }
 
-  public ConfigProperty getSelectedConfig()
-  {
-    return selectedConfig;
-  }
-
-  public void setSelectedConfig(ConfigProperty selectedConfig)
-  {
-    this.selectedConfig = selectedConfig;
-  }
-
   public List<ConfigProperty> getFilteredConfigs()
   {
     return filteredConfigs;
@@ -52,11 +37,5 @@ public class AdvancedConfigBean
   public void setFilteredConfigs(List<ConfigProperty> filteredConfigs)
   {
     this.filteredConfigs = filteredConfigs;
-  }
-
-  public void onRowSelect(SelectEvent event)
-  {
-    FacesMessage msg = new FacesMessage("Config Selected", ((ConfigProperty) event.getObject()).getKey());
-    FacesContext.getCurrentInstance().addMessage(null, msg);
   }
 }
