aafede313090a2abe92bd970d62e4c195c5d37d7
Lukas Lieb <lukas.lieb@axonivy.com>
2019-04-03 11:35:59 +0200

    fix bug on applications view if no app is deployed

--- engine-cockpit/src/ch/ivyteam/enginecockpit/ApplicationDetailBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/ApplicationDetailBean.java
@@ -2,6 +2,7 @@ package ch.ivyteam.enginecockpit;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,7 @@ import javax.faces.event.ValueChangeEvent;
 import org.apache.commons.lang3.StringUtils;
 
 import ch.ivyteam.enginecockpit.model.Application;
+import ch.ivyteam.enginecockpit.model.EmailSettings;
 import ch.ivyteam.enginecockpit.model.Property;
 import ch.ivyteam.enginecockpit.model.SecuritySystem;
 import ch.ivyteam.enginecockpit.util.SecuritySystemConfig;
@@ -30,6 +32,8 @@ public class ApplicationDetailBean
   private List<Property> properties;
   private List<String> environments;
   
+  private EmailSettings emailSettings;
+  
   private ManagerBean managerBean;
   
   public ApplicationDetailBean()
@@ -37,6 +41,7 @@ public class ApplicationDetailBean
     FacesContext context = FacesContext.getCurrentInstance();
     managerBean = context.getApplication().evaluateExpressionGet(context, "#{managerBean}",
             ManagerBean.class);
+    reloadEmailSettings();
   }
   
   public void setAppName(String appName)
@@ -135,5 +140,27 @@ public class ApplicationDetailBean
   {
     app.setSecuritySystem(event.getNewValue().toString());
     security = initSecuritySystem(getAppName());
+  }
+  
+  public void reloadEmailSettings()
+  {
+    emailSettings = new EmailSettings(managerBean.getSelectedIApplication());
+    emailSettings.setNotificationCheckboxRender(false);
+  }
+  
+  public EmailSettings getEmailSettings()
+  {
+    return emailSettings;
+  }
+  
+  public void saveEmailSettings()
+  {
+    IApplication iApp = managerBean.getSelectedIApplication();
+    Locale language = emailSettings.getLanguageLocale();
+    iApp.setDefaultEMailLanguage(language);
+    iApp.setDefaultEMailNotifcationSettings(
+            emailSettings.saveEmailSettings(iApp.getDefaultEMailNotifcationSettings()));
+    FacesContext.getCurrentInstance().addMessage("emailSaveSuccess",
+            new FacesMessage("User email changes saved"));
   }
 }
