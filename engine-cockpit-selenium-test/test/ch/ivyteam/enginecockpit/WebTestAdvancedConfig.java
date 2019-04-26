b902ad75299a0b04bb57be872441fbd62b7240ca
Lukas Lieb <lukas.lieb@axonivy.com>
2019-04-26 08:54:52 +0200

    XIVY-3037 add config add/edit/delete test

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestAdvancedConfig.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestAdvancedConfig.java
@@ -43,6 +43,46 @@ public class WebTestAdvancedConfig extends WebTestBase
     webAssertThat(() -> assertThat(driver.findElementById("card:newConfigurationForm:newConfigurationKeyMessage").isDisplayed()).isTrue());
   }
   
+  @Test
+  void testNewEditAndDeleteConfig(FirefoxDriver driver)
+  {
+    toAdvancedConfig(driver);
+    driver.findElementById("card:newConfigBtn").click();
+    saveScreenshot(driver, "new_config_model");
+    webAssertThat(() -> assertThat(driver.findElementById("card:newConfigurationModal").isDisplayed()).isTrue());
+    
+    driver.findElementById("card:newConfigurationForm:newConfigurationKey").sendKeys("testKey");
+    driver.findElementById("card:newConfigurationForm:newConfigurationValue").sendKeys("testValue");
+    driver.findElementById("card:newConfigurationForm:savenewConfiguration").click();
+    saveScreenshot(driver, "save_new_config");
+    webAssertThat(() -> assertThat(driver.findElementByXPath("//*[@class='config-name'][text()='testKey']/../../td[2]").getText()).contains("testValue"));
+    
+    String eleNumber = driver.findElementByXPath("//*[@class='config-name'][text()='testKey']/../..").getAttribute("data-ri");
+    String id = "card:form:advancedConfigTable:" + eleNumber;
+    driver.findElementById(id + ":editConfigBtn").click();
+    saveScreenshot(driver, "edit_config");
+    webAssertThat(() -> assertThat(driver.findElementById("card:editConfigurationModal").isDisplayed()).isTrue());
+    webAssertThat(() -> assertThat(driver.findElementById("card:editConfigurationForm:editConfigurationKey").getText()).isEqualTo("testKey"));
+    webAssertThat(() -> assertThat(driver.findElementById("card:editConfigurationForm:editConfigurationValue").getAttribute("value")).isEqualTo("testValue"));
+    
+    driver.findElementById("card:editConfigurationForm:editConfigurationValue").clear();
+    driver.findElementById("card:editConfigurationForm:editConfigurationValue").sendKeys("newValue");
+    driver.findElementById("card:editConfigurationForm:saveEditConfiguration").click();
+    saveScreenshot(driver, "save_edit_config");
+    webAssertThat(() -> assertThat(driver.findElementByXPath("//*[@class='config-name'][text()='testKey']/../../td[2]").getText()).contains("newValue"));
+  
+    driver.findElementById(id + ":tasksButton").click();
+    webAssertThat(() -> assertThat(driver.findElementById(id + ":activityMenu").isDisplayed()).isTrue());
+    driver.findElementById(id + ":deleteConfigBtn").click();
+    saveScreenshot(driver, "delete_config");
+    webAssertThat(() -> assertThat(driver.findElementById("card:form:deleteConfigConfirmDialog").isDisplayed()).isTrue());
+    
+    driver.findElementById("card:form:deleteConfigConfirmYesBtn").click();
+    saveScreenshot(driver, "delete_config_yes");
+    webAssertThat(() -> assertThat(driver.findElementsByClassName("config-name")
+            .stream().map(e -> e.getText()).anyMatch(t -> t.equals("testKey"))).isFalse());
+  }
+  
   private void toAdvancedConfig(FirefoxDriver driver)
   {
     login(driver);
