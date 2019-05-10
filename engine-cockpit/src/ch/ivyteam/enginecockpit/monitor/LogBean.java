aab2e757accdd96452034d8f43298fedf7f06b10
Lukas Lieb <lukas.lieb@axonivy.com>
2019-05-10 08:15:21 +0200

    implement date chooser for log viewer

--- engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/LogBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/LogBean.java
@@ -1,7 +1,13 @@
 package ch.ivyteam.enginecockpit.monitor;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.apache.commons.lang3.time.DateUtils;
 
 import ch.ivyteam.enginecockpit.model.LogView;
 
@@ -13,12 +19,24 @@ public class LogBean
   private LogView ivyLog;
   private LogView consoleLog;
   private LogView configLog;
+  private Date date;
   
   public LogBean()
   {
-    ivyLog = new LogView("ivy.log");
-    consoleLog = new LogView("console.log");
-    configLog = new LogView("config.log");
+    initLogFiles();
+  }
+
+  private void initLogFiles()
+  {
+    String formatDate = "";
+    if (date != null && !DateUtils.isSameDay(date, getToday()))
+    {
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+      formatDate = "." + format.format(date);
+    }
+    ivyLog = new LogView("ivy.log" + formatDate);
+    consoleLog = new LogView("console.log" + formatDate);
+    configLog = new LogView("config.log" + formatDate);
   }
   
   public LogView getIvyLog()
@@ -34,6 +52,22 @@ public class LogBean
   public LogView getConfigLog()
   {
     return configLog;
+  }
+  
+  public void setDate(Date date)
+  {
+    this.date = date;
+    initLogFiles();
+  }
+  
+  public Date getDate()
+  {
+    return date;
+  }
+  
+  public Date getToday()
+  {
+    return Calendar.getInstance().getTime();
   }
   
 }
