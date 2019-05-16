7f3ea21c83a6eb098013eed13574bd9dd67f0cf4
Oliver Heer <oliver.heer@axonivy.com>
2019-05-16 14:56:06 +0200

    XIVY-3029 Remove validation check in tests

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestVariables.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestVariables.java
@@ -74,8 +74,6 @@ public class WebTestVariables extends WebTestBase
     driver.findElementById("newGlobalVarForm:saveNewGlobalVar").click();
 
     webAssertThat(() -> assertThat(driver.findElementById("newGlobalVarForm:newGlobalVarNameMessage").getText()).isEqualTo("Name is required"));
-    webAssertThat(() -> assertThat(driver.findElementById("newGlobalVarForm:newGlobalVarDescMessage").getText()).isEqualTo("Description is required"));
-    
     driver.findElementById("newGlobalVarForm:cancelNewGlobalVar").click();
     webAssertThat(() -> assertThat(driver.findElementById("newGlobalVarModal").isDisplayed()).isFalse());
   }
