48de5d83880b241ddc416a5e4f95717c3dbcef6f
Lukas Lieb <lukas.lieb@axonivy.com>
2019-05-31 11:08:21 +0200

    XIVY-3036 fix typos

--- engine-cockpit/src/ch/ivyteam/enginecockpit/fileupload/FileUploadService.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/fileupload/FileUploadService.java
@@ -33,6 +33,6 @@ public class FileUploadService
     {
       return Response.status(500).entity(ex.getMessage()).build();
     }
-    return Response.status(200).entity("Successfully uploaded licence").build();
+    return Response.status(200).entity("Successful uploaded licence").build();
   }
 }
