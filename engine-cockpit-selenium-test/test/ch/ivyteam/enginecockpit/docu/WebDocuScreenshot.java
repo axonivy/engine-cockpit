7a5201fd2b521431fe7fd8c40380d0e9a01357ca
Lukas Lieb <lukas.lieb@axonivy.com>
2019-08-12 11:31:38 +0200

    change growl autoUpdate to tag, test fix

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
@@ -140,7 +140,8 @@ public class WebDocuScreenshot extends WebBase
   
   private void populateBusinessCalendar(FirefoxDriver driver)
   {
-    driver.get(EngineCockpitUrl.base() + "/pro/test/engine-cockpit-test-data/16AD3F265FFA55DD/start.ivp");
+    String app = EngineCockpitUrl.isDesignerApp() ? EngineCockpitUrl.DESIGNER_APP : "test";
+    driver.get(EngineCockpitUrl.base() + "/pro/" + app + "/engine-cockpit-test-data/16AD3F265FFA55DD/start.ivp");
   }
   
 }
