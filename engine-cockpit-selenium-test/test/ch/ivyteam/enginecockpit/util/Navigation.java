177de78ffc0c8665968ed368e17e322bd1fedf33
Lukas Lieb <lukas.lieb@axonivy.com>
2019-10-03 11:57:52 +0200

    fix merge, revert some navigation changes

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/util/Navigation.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/util/Navigation.java
@@ -1,8 +1,8 @@
 package ch.ivyteam.enginecockpit.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import ch.ivyteam.enginecockpit.WebBase;
@@ -31,179 +31,192 @@ public class Navigation
   public static void toDashboard(FirefoxDriver driver)
   {
     toMenu(driver, DASHBOARD_MENU);
-    assertCurrentUrlEndsWith(driver, "dashboard.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("dashboard.xhtml"));
   }
   
   public static void toApplications(FirefoxDriver driver)
   {
     toMenu(driver, APPLICATIONS_MENU);
-    assertCurrentUrlEndsWith(driver, "applications.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("applications.xhtml"));
   }
   
   public static void toApplicationDetail(FirefoxDriver driver, String appName)
   {
     Navigation.toApplications(driver);
     driver.findElementByXPath("//span[@class='activity-name'][text()='" + appName + "']").click();
-    assertCurrentUrlEndsWith(driver, "application-detail.xhtml?appName=" + appName);
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("application-detail.xhtml?appName=" + appName)); 
   }
   
   public static void toSecuritySystem(FirefoxDriver driver)
   {
     toSubMenu(driver, SECURITY_MENU, SECURITY_SYSTEM_MENU);
-    assertCurrentUrlEndsWith(driver, "securitysystem.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("securitysystem.xhtml"));
   }
   
   public static void toSecuritySystemDetail(FirefoxDriver driver, String secSystemName)
   {
     Navigation.toSecuritySystem(driver);
-    WebBase.waitUntilElementClickable(driver, By.xpath("//span[@class='security-name'][text()='" + secSystemName + "']")).click();
-    assertCurrentUrlEndsWith(driver, "security-detail.xhtml?securitySystemName=" + secSystemName);
+    waitBeforeClick(driver, By.xpath("//span[@class='security-name'][text()='" + secSystemName + "']"));
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("security-detail.xhtml?securitySystemName=" + secSystemName)); 
   }
 
   public static void toVariables(FirefoxDriver driver)
   {
     toSubMenu(driver, CONFIGURATION_MENU, VARIABLES_MENU);
-    assertCurrentUrlEndsWith(driver, "global-variables.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("global-variables.xhtml"));
   }
   
   public static void toBusinessCalendar(FirefoxDriver driver)
   {
     toSubMenu(driver, CONFIGURATION_MENU, BUSINESS_CALENDAR_MENU);
-    assertCurrentUrlEndsWith(driver, "businesscalendar.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("businesscalendar.xhtml"));
   }
   
   public static void toBusinessCalendarDetail(FirefoxDriver driver, String calendarName)
   {
     Navigation.toBusinessCalendar(driver);
     driver.findElementByXPath("//*[@id=\"calendarTree\"]//a[contains(@id, 'calendarNode')][text()='" + calendarName + "']").click();
-    assertCurrentUrlEndsWith(driver, "businesscalendar-detail.xhtml?calendarName=" + calendarName);
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("businesscalendar-detail.xhtml?calendarName=" + calendarName));
   }
   
   public static void toUsers(FirefoxDriver driver)
   {
     toSubMenu(driver, SECURITY_MENU, SECURITY_USER_MENU);
-    assertCurrentUrlEndsWith(driver, "users.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("users.xhtml"));
   }
   
-  public static void toUserDetail(FirefoxDriver driver, String name)
+  public static void toUserDetail(FirefoxDriver driver, String userName)
   {
     Navigation.toUsers(driver);
-    WebBase.waitUntilElementClickable(driver, 
-            By.xpath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='user-name'][text()='" + name + "']")).click();
-    assertCurrentUrlEndsWith(driver, "userdetail.xhtml?userName=" + name);
+    driver.findElementsByXPath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='user-name'][text()='" + userName + "']").stream()
+            .filter(e -> checkIfCorrectElement(e))
+            .forEach(e -> e.click());
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("userdetail.xhtml?userName=" + userName)); 
   }
   
   public static void toRoles(FirefoxDriver driver)
   {
     toSubMenu(driver, SECURITY_MENU, SECURITY_ROLES_MENU);
-    assertCurrentUrlEndsWith(driver, "roles.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("roles.xhtml"));
   }
   
-  public static void toRoleDetail(FirefoxDriver driver, String name)
+  public static void toRoleDetail(FirefoxDriver driver, String roleName)
   {
     Navigation.toRoles(driver);
-    WebBase.waitUntilElementClickable(driver, 
-            By.xpath("//div[contains(@class, 'ui-tabs-panel')]//a[@class='role-name'][text()='" + name + "']")).click();
-    assertCurrentUrlEndsWith(driver, "roledetail.xhtml?roleName=" + name);
+    driver.findElementsByXPath("//div[contains(@class, 'ui-tabs-panel')]//a[@class='role-name'][text()='" + roleName + "']").stream()
+            .filter(e -> checkIfCorrectElement(e))
+            .forEach(e -> e.click());
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("roledetail.xhtml?roleName=" + roleName)); 
   }
   
   public static void toSearchEngine(FirefoxDriver driver)
   {
     toSubMenu(driver, SERVICES_MENU, SERVICES_SEARCH_ENGINE);
-    assertCurrentUrlEndsWith(driver, "searchengine.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("searchengine.xhtml"));
   }
   
   public static void toEmail(FirefoxDriver driver)
   {
     toSubMenu(driver, SERVICES_MENU, SERVICES_EMAIL_MENU);
-    assertCurrentUrlEndsWith(driver, "email.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("email.xhtml"));
   }
   
   public static void toExternalDatabases(FirefoxDriver driver)
   {
     toSubMenu(driver, SERVICES_MENU, SERVICES_DATABASES_MENU);
-    assertCurrentUrlEndsWith(driver, "externaldatabases.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("externaldatabases.xhtml"));
   }
   
-  public static void toExternalDatabaseDetail(FirefoxDriver driver, String name)
+  public static void toExternalDatabaseDetail(FirefoxDriver driver, String databaseName)
   {
     Navigation.toExternalDatabases(driver);
-    WebBase.waitUntilElementClickable(driver, 
-            By.xpath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='database-name'][text()='" + name + "']")).click();
-    assertCurrentUrlEndsWith(driver, "externaldatabasedetail.xhtml?databaseName=" + name);
+    driver.findElementsByXPath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='database-name'][text()='" + databaseName + "']").stream()
+            .filter(e -> checkIfCorrectElement(e))
+            .forEach(e -> e.click());
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("externaldatabasedetail.xhtml?databaseName=" + databaseName)); 
   }
   
   public static void toRestClients(FirefoxDriver driver)
   {
     toSubMenu(driver, SERVICES_MENU, SERVICES_RESTCLIENTS_MENU);
-    assertCurrentUrlEndsWith(driver, "restclients.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("restclients.xhtml"));
   }
   
-  public static void toRestClientDetail(FirefoxDriver driver, String name)
+  public static void toRestClientDetail(FirefoxDriver driver, String restClientName)
   {
     Navigation.toRestClients(driver);
-    WebBase.waitUntilElementClickable(driver, 
-            By.xpath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='restclient-name'][text()='" + name + "']")).click();
-    assertCurrentUrlEndsWith(driver, "restclientdetail.xhtml?restClientName=" + name);
+    driver.findElementsByXPath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='restclient-name'][text()='" + restClientName + "']").stream()
+            .filter(e -> checkIfCorrectElement(e))
+            .forEach(e -> e.click());
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("restclientdetail.xhtml?restClientName=" + restClientName)); 
   }
   
   public static void toWebservices(FirefoxDriver driver)
   {
     toSubMenu(driver, SERVICES_MENU, SERVICES_WEBSERVICES_MENU);
-    assertCurrentUrlEndsWith(driver, "webservices.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("webservices.xhtml"));
   }
   
-  public static void toWebserviceDetail(FirefoxDriver driver, String name)
+  public static void toWebserviceDetail(FirefoxDriver driver, String webserviceName)
   {
     Navigation.toWebservices(driver);
-    WebBase.waitUntilElementClickable(driver, 
-            By.xpath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='webservice-name'][text()='" + name + "']")).click();
-    assertCurrentUrlContains(driver, "webservicedetail.xhtml?webserviceId=");
+    driver.findElementsByXPath("//div[contains(@class, 'ui-tabs-panel')]//span[@class='webservice-name'][text()='" + webserviceName + "']").stream()
+            .filter(e -> checkIfCorrectElement(e))
+            .forEach(e -> e.click());
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().contains("webservicedetail.xhtml?webserviceId=")); 
   }
   
   public static void toSystemConfig(FirefoxDriver driver)
   {
     toMenu(driver, SYSTEM_CONFIG_MENU);
-    assertCurrentUrlEndsWith(driver, "systemconfig.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("systemconfig.xhtml"));
   }
   
   public static void toMonitor(FirefoxDriver driver)
   {
     toMenu(driver, MONITOR_MENU);
-    assertCurrentUrlEndsWith(driver, "monitor.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().endsWith("monitor.xhtml"));
   }
   
   public static void toLogs(FirefoxDriver driver)
   {
     toMenu(driver, LOGS_MENU);
-    assertCurrentUrlContains(driver, "logs.xhtml");
+    WebBase.webAssertThat(() -> driver.getCurrentUrl().contains("logs.xhtml"));
   }
   
   private static void toMenu(FirefoxDriver driver, By menuItemPath)
   {
     driver.findElement(menuItemPath).click();
-    WebBase.webAssertThat(() -> assertThat(driver.findElement(menuItemPath).findElement(By.xpath("./..")).
-            getAttribute("class")).contains("active-menuitem"));
+    WebBase.webAssertThat(() -> driver.findElement(menuItemPath).findElement(By.xpath("./..")).
+            getAttribute("class").contains("active-menuitem"));
   }
   
   private static void toSubMenu(FirefoxDriver driver, By menuItemPath, By subMenuItemPath)
   {
     if(!driver.findElement(subMenuItemPath).isDisplayed()) {
-      WebBase.waitUntilElementClickable(driver, menuItemPath).click();
+      waitBeforeClick(driver, menuItemPath);
     }
-    WebBase.waitUntilElementClickable(driver, subMenuItemPath).click();
-    WebBase.webAssertThat(() -> assertThat(driver.findElement(subMenuItemPath).findElement(By.xpath("./..")).
-            getAttribute("class")).contains("active-menuitem"));
+    waitBeforeClick(driver, subMenuItemPath);
+    WebBase.webAssertThat(() -> driver.findElement(subMenuItemPath).findElement(By.xpath("./..")).
+            getAttribute("class").contains("active-menuitem"));
   }
   
-  private static void assertCurrentUrlEndsWith(FirefoxDriver driver, String page)
+  private static boolean checkIfCorrectElement(WebElement element)
   {
-    WebBase.webAssertThat(() -> assertThat(driver.getCurrentUrl()).endsWith(page));
+    try
+    {
+      return element.isDisplayed();
+    }
+    catch (StaleElementReferenceException e)
+    {
+      return false;
+    }
   }
   
-  private static void assertCurrentUrlContains(FirefoxDriver driver, String page)
+  private static void waitBeforeClick(FirefoxDriver driver, By element)
   {
-    WebBase.webAssertThat(() -> assertThat(driver.getCurrentUrl()).contains(page));
+    WebBase.webAssertThat(() -> driver.findElement(element).isDisplayed());
+    driver.findElement(element).click();
   }
   
 }
