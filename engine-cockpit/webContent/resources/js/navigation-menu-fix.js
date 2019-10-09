f44e7980efef1f2ed191398bf84cabc85d224299
Lukas Lieb <lukas.lieb@axonivy.com>
2019-10-09 16:32:49 +0200

    remove possibility to toogle menu -> always wide now

--- engine-cockpit/webContent/resources/js/navigation-menu-fix.js
+++ engine-cockpit/webContent/resources/js/navigation-menu-fix.js
@@ -21,54 +21,28 @@ $(document).ready(function() {
     if (PF('sidebar_menu') == null) {
       var timeoutID = window.setTimeout(initFix, 10);
     } else {
-      PF('sidebar_menu').saveMenuState = function(){
-        if(this.wrapper.hasClass('layout-wrapper-static'))
-          $.cookie('serenity_menu_static', 'serenity_menu_static', {path: '/'});
-        else
-          $.removeCookie('serenity_menu_static', {path: '/'});
-      };
-    
-      PF('sidebar_menu').restoreMenuState = function(){
-        var sidebarCookie = $.cookie('serenity_menu_static');
-        if(sidebarCookie) {
-          this.wrapper.addClass('layout-wrapper-static layout-wrapper-static-restore');
-        }
-      }
-    
-      PF('sidebar_menu').anchorButton.on('click', function(e) {
-        var menu = $.cookie('cockpit_menu_default');
-        if (menu == null) {
-          $.cookie('cockpit_menu_default', 'cockpit_menu_default', {path: '/'});
-        }
-      });
-
-      checkDefaultMenuState();
-      highlightMenuItem();
-
-      function highlightMenuItem() {
-        $.removeCookie('serenity_expandeditems', {path: '/'});
-        var page = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1);
-        page = searchCorrectMenuItemPage(page);
-        $('#menuform>ul>li>a').each(function() {
-          var menuItemPage = $(this).attr('href');
-          var menuItemId = $(this).parent().attr('id');
-          if (menuItemPage != "#") {
-            if (menuItemPage.startsWith(page)) {
-              activateMenuItem(menuItemId);
-            }
-          }
-          else {
-            $(this).parent().find('ul>li>a').each(function() {
-              var subMenuItemPage = $(this).attr('href');
-              var subMenuItemId = $(this).parent().attr('id');
-              if (subMenuItemPage.startsWith(page)) {
-                activateMenuItem(menuItemId);
-                activateMenuItem(subMenuItemId);
-              }
-            });
-          }
-        });
-      }
+      $.removeCookie('serenity_expandeditems', {path: '/'});
+	  var page = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1);
+	  page = searchCorrectMenuItemPage(page);
+	  $('#menuform>ul>li>a').each(function() {
+	    var menuItemPage = $(this).attr('href');
+	    var menuItemId = $(this).parent().attr('id');
+	    if (menuItemPage != "#") {
+	      if (menuItemPage.startsWith(page)) {
+	        activateMenuItem(menuItemId);
+	      }
+	    }
+	    else {
+	      $(this).parent().find('ul>li>a').each(function() {
+	        var subMenuItemPage = $(this).attr('href');
+	        var subMenuItemId = $(this).parent().attr('id');
+	        if (subMenuItemPage.startsWith(page)) {
+	          activateMenuItem(menuItemId);
+	          activateMenuItem(subMenuItemId);
+	        }
+	      });
+	    }
+	  });
 
       function searchCorrectMenuItemPage(value) {
         var map = {
@@ -90,14 +64,6 @@ $(document).ready(function() {
         var submenu = menuitem.children('ul');
         if(submenu.length) {
           submenu.show();
-        }
-      }
-      
-      function checkDefaultMenuState() {
-        var menu = $.cookie('cockpit_menu_default');
-        if (menu == null) {
-          $.cookie('serenity_menu_static', 'serenity_menu_static', {path: '/'});
-          PF('sidebar_menu').restoreMenuState();
         }
       }
     }
