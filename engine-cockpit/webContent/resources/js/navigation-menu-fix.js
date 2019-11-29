6e338357ea18fa762e8b32b20f17ebe5e5ee4a6a
Lukas Lieb <lukas.lieb@axonivy.com>
2019-11-29 13:48:46 +0100

    fix licence upload on wizard in ie (polyfill)

--- engine-cockpit/webContent/resources/js/navigation-menu-fix.js
+++ engine-cockpit/webContent/resources/js/navigation-menu-fix.js
@@ -1,21 +1,3 @@
-if (!String.prototype.startsWith) {
-  Object.defineProperty(String.prototype, 'startsWith', {
-    value: function(search, pos) {
-      pos = !pos || pos < 0 ? 0 : +pos;
-      return this.substring(pos, pos + search.length) === search;
-    }
-  });
-}
-
-if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function(search, this_len) {
-	if (this_len === undefined || this_len > this.length) {
-	  this_len = this.length;
-	}
-	return this.substring(this_len - search.length, this_len) === search;
-  };
-}
-
 $(document).ready(function() {
   function initFix() {
     if (PF('sidebar_menu') == null) {
