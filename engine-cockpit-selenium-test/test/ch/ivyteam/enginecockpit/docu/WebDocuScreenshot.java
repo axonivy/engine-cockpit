53d7e5ddfe20f00c3d75b23c37aead1e618da823
Lukas Lieb <lukas.lieb@axonivy.com>
2020-04-14 09:24:28 +0000

    Merged in feature/dialog-screenshots (pull request #143)
    Feature/dialog screenshots
    
    Approved-by: Christian Strebel

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
@@ -4,19 +4,33 @@ import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.executeJs;
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
 import static com.axonivy.ivy.webtest.engine.EngineUrl.DESIGNER;
 import static com.axonivy.ivy.webtest.engine.EngineUrl.isDesigner;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Rectangle;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 
 import ch.ivyteam.enginecockpit.setupwizard.WebTestWizard;
+import ch.ivyteam.enginecockpit.system.WebTestSystemDb;
 import ch.ivyteam.enginecockpit.util.EngineCockpitUtil;
 import ch.ivyteam.enginecockpit.util.Navigation;
 
@@ -54,14 +68,21 @@ public class WebDocuScreenshot
   {
     login();
     takeScreenshot("engine-cockpit-dashboard", new Dimension(SCREENSHOT_WIDTH, 800));
+    takeDialogScreenshot("engine-cockpit-dialog-licence", By.id("uploadLicenceBtn"));
+    takeDialogScreenshot("engine-cockpit-dialog-test-mail", By.id("mailConfigForm:openTestMailBtn"));
     Navigation.toApplications();
     takeScreenshot("engine-cockpit-applications", new Dimension(SCREENSHOT_WIDTH, 500));
+    takeDialogScreenshot("engine-cockpit-dialog-new-app", By.id("card:form:createApplicationBtn"));
     Navigation.toApplicationDetail(isDesigner() ? DESIGNER : "test");
     takeScreenshot("engine-cockpit-application-detail", new Dimension(SCREENSHOT_WIDTH, 900));
+    takeDialogScreenshot("engine-cockpit-dialog-change-security", By.id("appDetailSecurityForm:changeSecuritySystem"));
+    takeDialogScreenshot("engine-cockpit-dialog-deploy-app", By.id("appDetailInfoForm:showDeployment"));
     Navigation.toSecuritySystem();
     takeScreenshot("engine-cockpit-security-system", new Dimension(SCREENSHOT_WIDTH, 500));
+    takeDialogScreenshot("engine-cockpit-dialog-new-security", By.id("card:form:createSecuritySystemBtn"));
     Navigation.toSecuritySystemDetail("test-ad");
     takeScreenshot("engine-cockpit-security-system-detail", new Dimension(SCREENSHOT_WIDTH, 900));
+    takeDialogScreenshot("engine-cockpit-dialog-ldap-browser", By.id("securitySystemBindingForm:browseDefaultContext"));
     Navigation.toUsers();
     takeScreenshot("engine-cockpit-users", new Dimension(SCREENSHOT_WIDTH, 600));
     Navigation.toUserDetail("foo");
@@ -94,6 +115,7 @@ public class WebDocuScreenshot
     takeScreenshot("engine-cockpit-rest-client-detail", new Dimension(SCREENSHOT_WIDTH, 600));
     Navigation.toAdmins();
     takeScreenshot("engine-cockpit-system-admins", new Dimension(SCREENSHOT_WIDTH, 500));
+    takeDialogScreenshot("engine-cockpit-dialog-new-admin", By.id("addAdminForm:newAdminBtn"));
     Navigation.toSystemDb();
     takeScreenshot("engine-cockpit-system-database", new Dimension(SCREENSHOT_WIDTH, 900));
     Navigation.toSystemConfig();
@@ -112,7 +134,38 @@ public class WebDocuScreenshot
     WebTestWizard.navigateToStep("Web Server");
     takeScreenshot("engine-cockpit-setup-webserver", new Dimension(SCREENSHOT_SETUP_WIDTH, 550));
     WebTestWizard.navigateToStep("System Database");
+    WebTestSystemDb.assertSystemDbCreationDialog();
+    takeDialogScreenshot("engine-cockpit-dialog-setup-systemdb-create");
     takeScreenshot("engine-cockpit-setup-systemdb", new Dimension(SCREENSHOT_SETUP_WIDTH, 900));
+    takeDialogScreenshot("engine-cockpit-dialog-setup-finish", By.id("finishWizard"));
+  }
+
+  private void takeDialogScreenshot(String screenshotName)
+  {
+    SelenideElement dialog = $$(".ui-dialog").find(visible);
+    Rectangle dialogRect = dialog.getRect();
+    Point dialogCoordiantes = dialog.getCoordinates().inViewPort();
+    File dialogFile = new File(Selenide.screenshot(screenshotName));
+    try
+    {
+      BufferedImage dialogScreenshot = ImageIO.read(dialogFile).getSubimage(
+              dialogCoordiantes.getX(), 
+              dialogCoordiantes.getY(), 
+              dialogRect.getWidth(), 
+              dialogRect.getHeight() + 1);
+      ImageIO.write(dialogScreenshot, "png", dialogFile);
+    }
+    catch (IOException ex)
+    {
+      throw new RuntimeException("Error while try crop screenshot to dialog size: ", ex);
+    }
+    $$(".ui-dialog-titlebar-close").find(visible).click();
+  }
+
+  private void takeDialogScreenshot(String screenshotName, By dialogOpenBtn)
+  {
+    $(dialogOpenBtn).shouldBe(visible).click();
+    takeDialogScreenshot(screenshotName);
   }
 
   public void takeScreenshot(String fileName, Dimension size)
