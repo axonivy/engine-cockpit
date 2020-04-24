d8aab64f7b7aa27693f37aba57d163076155e697
Lukas Lieb <lukas.lieb@axonivy.com>
2020-04-24 08:33:50 +0200

    refactor reportbean

--- engine-cockpit/src/ch/ivyteam/enginecockpit/ReportBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/ReportBean.java
@@ -10,6 +10,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 
+import org.apache.commons.io.FileUtils;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
@@ -18,6 +19,7 @@ import ch.ivyteam.enginecockpit.util.DownloadUtil;
 import ch.ivyteam.enginecockpit.util.UrlUtil;
 import ch.ivyteam.ivy.application.IApplicationConfigurationManager;
 import ch.ivyteam.ivy.application.restricted.ApplicationConfigurationDumper;
+import ch.ivyteam.ivy.environment.Ivy;
 import ch.ivyteam.ivy.error.restricted.ErrorReport;
 import ch.ivyteam.ivy.error.restricted.IDumper;
 import ch.ivyteam.ivy.persistence.db.ISystemDatabasePersistencyService;
@@ -42,27 +44,44 @@ public class ReportBean
   
   public StreamedContent getErrorReport() throws IOException
   {
+    String errorReport = createErrorReport();
+    Path tempDirectory = collectReportData(errorReport);
+    
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    DownloadUtil.zipDir(tempDirectory, out);
+    FileUtils.deleteDirectory(tempDirectory.toFile());
+    return new DefaultStreamedContent(new ByteArrayInputStream(out.toByteArray()), "application/zip", "errorReport.zip");
+  }
+
+  private String createErrorReport()
+  {
     IDumper[] dumpers = ErrorReport.addStandardDumpers(false,
             new ApplicationConfigurationDumper(applicationConfigurationManager),
             new PersistencyDumper(systemDatabasePersistencyService));
 
     String errorReport = ErrorReport.createErrorReport(dumpers);
+    return errorReport;
+  }
+
+  private Path collectReportData(String errorReport) throws IOException
+  {
     Path tempDirectory = Files.createTempDirectory("errorReport");
     Files.writeString(Files.createFile(tempDirectory.resolve("report.txt")), errorReport);
     Files.walk(UrlUtil.getLogDir().toPath())
             .filter(Files::isRegularFile)
-            .forEach(log -> {
-              try
-              {
-                Files.copy(log, tempDirectory.resolve(log.getFileName()));
-              }
-              catch (IOException ex)
-              {
-              }
-            });
-    
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    DownloadUtil.zipDir(tempDirectory, out);
-    return new DefaultStreamedContent(new ByteArrayInputStream(out.toByteArray()), "application/zip", "errorReport.zip");
+            .forEach(log -> copyLogFile(log, tempDirectory));
+    return tempDirectory;
+  }
+
+  private void copyLogFile(Path log, Path tempDirectory)
+  {
+    try
+    {
+      Files.copy(log, tempDirectory.resolve(log.getFileName()));
+    }
+    catch (IOException ex)
+    {
+      Ivy.log().info("Couldn't copy file '" + log + "' to tempDir '" + tempDirectory + "': ", ex);
+    }
   }
 }
