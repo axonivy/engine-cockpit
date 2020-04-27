be7a43dc444daea321c38e3a4285666f9fbf35aa
Alexander Suter <alexander.suter@axonivy.com>
2020-04-27 23:45:06 +0200

    XIVY-3573 Use MailClient

--- engine-cockpit/src/ch/ivyteam/enginecockpit/util/EmailUtil.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/util/EmailUtil.java
@@ -1,59 +1,31 @@
 package ch.ivyteam.enginecockpit.util;
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
-import ch.ivyteam.ivy.configuration.restricted.IConfiguration;
-import ch.ivyteam.ivy.email.EmailConstants.EmailEncryption;
-import ch.ivyteam.ivy.email.EmailSetupConfiguration;
-import ch.ivyteam.ivy.email.SimpleMailSender;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.process.model.element.activity.service.EMailConfig;
-import ch.ivyteam.ivy.process.model.element.activity.value.email.Attachments;
-import ch.ivyteam.ivy.process.model.element.activity.value.email.Headers;
-import ch.ivyteam.ivy.process.model.element.activity.value.email.Headers.HeaderFields;
-import ch.ivyteam.ivy.process.model.element.value.MacroExpression;
+import ch.ivyteam.ivy.email.MailClient;
+import ch.ivyteam.ivy.email.MailMessage;
 
 @SuppressWarnings("restriction")
 public class EmailUtil
 {
   private static final Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
-  public static final String HOST = "EMail.Server.Host";
-  public static final String PORT = "EMail.Server.Port";
-  public static final String MAIL_ADDRESS = "EMail.Server.MailAddress";
-  public static final String USER = "EMail.Server.User";
-  public static final String PASSWORD = "EMail.Server.Password";
-  public static final String ENCRYPTION_METHOD = "EMail.Server.EncryptionMethod";
   public static final String DAILYTASKSUMMARY_TRIGGERTIME = "EMail.DailyTaskSummary.TriggerTime";
-  
+
   public static void sendTestMail(String subject, String to, String message) throws Exception
   {
-    Map<HeaderFields, MacroExpression> rawHeader = new HashMap<>();
-    rawHeader.put(HeaderFields.SUBJECT, new MacroExpression(subject));
-    rawHeader.put(HeaderFields.TO, new MacroExpression(to));
-    rawHeader.put(HeaderFields.FROM, new MacroExpression(IConfiguration.get().getOrDefault(MAIL_ADDRESS)));
-    EMailConfig emailConfig = new EMailConfig(new Headers(rawHeader), new MacroExpression(message), new Attachments(), false);
+    try (var mailClient = MailClient.newMailClient())
+    {
+        var mailMessage = MailMessage.create()
+            .to(to)
+            .subject(subject)
+            .textContent(message)
+            .toMailMessage();
+        mailClient.send(mailMessage);
+    }
+  }
 
-    new SimpleMailSender(List.of(), emailConfig, Ivy.log(), getConfigEmailSetup()).sendMessage();
-  }
-  
-  private static EmailSetupConfiguration getConfigEmailSetup()
-  {
-    EmailSetupConfiguration emailSetupConfiguration = new EmailSetupConfiguration();
-    emailSetupConfiguration.setSmtpPort(IConfiguration.get().getOrDefault(PORT, int.class));
-    emailSetupConfiguration.setSmtpServer(IConfiguration.get().getOrDefault(HOST));
-    emailSetupConfiguration.setSmtpUser(IConfiguration.get().getOrDefault(USER));
-    emailSetupConfiguration.setSmtpPassword(IConfiguration.get().getOrDefault(PASSWORD));
-    emailSetupConfiguration.setSmtpEncryptionMethod(IConfiguration.get().getOrDefault(ENCRYPTION_METHOD, EmailEncryption.class));
-    return emailSetupConfiguration;
-  }
-  
   public static boolean validateEmailAddress(String email)
   {
     return EMAIL_REGEX.matcher(email).matches();
   }
-
 }
