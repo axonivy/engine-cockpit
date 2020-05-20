9314b4b784a4a0c82b040029e90502ace27119e4
Lukas Lieb <lukas.lieb@axonivy.com>
2020-05-20 15:30:02 +0200

    XIVY-4159 use adapter from core

--- engine-cockpit-test/src_test/ch/ivytea/enginecockpit/modal/TestProcessModelVersion.java
+++ engine-cockpit-test/src_test/ch/ivytea/enginecockpit/modal/TestProcessModelVersion.java
@@ -5,14 +5,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 import ch.ivyteam.enginecockpit.model.ProcessModelVersion;
+import ch.ivyteam.ivy.application.restricted.ProcessModelVersionAdapter;
 
+@SuppressWarnings("restriction")
 public class TestProcessModelVersion
 {
 
+  /** ISSUE XIVY-4159 Application overview not displayed due to NPE */
   @Test
   void emptyLibrary()
   {
-    ProcessModelVersion pmv = new ProcessModelVersion(new FakeProcessModelVersion());
+    ProcessModelVersion pmv = new ProcessModelVersion(new ProcessModelVersionAdapter());
     assertThat(pmv.getQualifiedVersion()).isEqualTo("Unknown version");
   }
   
