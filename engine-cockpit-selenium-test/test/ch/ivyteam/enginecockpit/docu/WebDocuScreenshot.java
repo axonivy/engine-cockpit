89d4f4395cfe37f0b2d73e2efcdb06302df57e9f
Lukas Lieb <lukas.lieb@axonivy.com>
2020-12-23 08:10:01 +0100

    change height of stable screenshot

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/docu/WebDocuScreenshot.java
@@ -125,7 +125,7 @@ public class WebDocuScreenshot
     Navigation.toLicence();
     takeScreenshot("engine-cockpit-licence", new Dimension(SCREENSHOT_WIDTH, 700));
     Navigation.toWebServer();
-    takeScreenshot("engine-cockpit-web-server", new Dimension(SCREENSHOT_WIDTH, 900));
+    takeScreenshot("engine-cockpit-web-server", new Dimension(SCREENSHOT_WIDTH, 600));
     Navigation.toSystemConfig();
     takeScreenshot("engine-cockpit-system-config", new Dimension(SCREENSHOT_WIDTH, 700));
     Navigation.toCluster();
