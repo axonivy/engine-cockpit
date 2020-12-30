87191bfdbf33450885371bcf1680de453bf765a1
Alexander Suter <alexander.suter@axonivy.com>
2020-12-30 08:47:17 +0100

    XIVY-5309 Fix role hierarchy view to not only show 3 roles in hierarchy

--- engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDataModel.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDataModel.java
@@ -103,8 +103,7 @@ public class RoleDataModel extends TreeView
     @SuppressWarnings("unused")
     private void ensureChildrenFetched()
     {
-      if (!childrenFetched && !member &&
-              role != null && getParent().isExpanded())
+      if (!childrenFetched && !member && role != null)
       {
         childrenFetched = true;
         var rolesLeft = addRolesToTree(role.getChildRoles(), false);
