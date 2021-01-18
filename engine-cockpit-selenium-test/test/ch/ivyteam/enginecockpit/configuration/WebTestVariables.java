e5e324444fe747f6fef57633ee12c1db6e189d30
Lukas Lieb <lukas.lieb@axonivy.com>
2021-01-18 17:01:11 +0100

    add and improve global var tests

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/configuration/WebTestVariables.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/configuration/WebTestVariables.java
@@ -1,19 +1,24 @@
 package ch.ivyteam.enginecockpit.configuration;
 
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.exactValue;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 
+import ch.ivyteam.enginecockpit.util.EnvironmentSwitch;
 import ch.ivyteam.enginecockpit.util.Navigation;
 import ch.ivyteam.enginecockpit.util.Tab;
+import ch.ivyteam.enginecockpit.util.Table;
 
 @IvyWebTest
 public class WebTestVariables
@@ -24,13 +29,54 @@ public class WebTestVariables
   {
     login();
     Navigation.toVariables();
-    Tab.switchToTab("test");
+    Tab.switchToDefault();
+    EnvironmentSwitch.switchToEnv("Default");
   }
   
   @Test
-  void testNewEditVariable()
+  void testInitialVariables()
   {
-    $(activeTabPanel() + "newGlobalVarBtn").click();
+    var table = variableTable();
+    table.firstColumnShouldBe(exactTexts("globVar", "PORTAL_DASHBOARD", "variable"));
+    assertEntry("globVar", "data", true);
+    assertEntry("variable", "hello", true);
+    EnvironmentSwitch.switchToEnv("test");
+    table.firstColumnShouldBe(exactTexts("globVar", "PORTAL_DASHBOARD", "variable"));
+    assertEntry("globVar", "test data", false);
+    assertEntry("variable", "hello from env", true);
+  }
+  
+  @Test
+  void editVariable()
+  {
+    String variable = "variable";
+    EnvironmentSwitch.switchToEnv("test");
+    assertEntry(variable, "hello from env", true);
+    editVariable(variable, "env override");
+    assertEntry(variable, "env override", false);
+    
+    EnvironmentSwitch.switchToEnv("Default");
+    assertEntry(variable, "hello", true);
+    editVariable(variable, "override");
+    assertEntry(variable, "override", false);
+    
+    EnvironmentSwitch.switchToEnv("test");
+    assertEntry(variable, "env override", false);
+    resetVariable(variable);
+    assertEntry(variable, "override", false); //should be "default"
+
+    EnvironmentSwitch.switchToEnv("Default");
+    assertEntry(variable, "override", false);
+    resetVariable(variable);
+    assertEntry(variable, "hello", true);
+  }
+
+  @Test
+  void testNewEditResetVariable()
+  {
+    var table = variableTable();
+    var entryCount = table.getFirstColumnEntries().size();
+    $(By.id(activeTabPanel() + "newGlobalVarBtn")).click();
 
     String name = "aName";
     String value = "aValue";
@@ -40,21 +86,49 @@ public class WebTestVariables
     $("#newGlobalVarForm\\:newGlobalVarValue").shouldBe(exactValue("")).sendKeys(value);
     $("#newGlobalVarForm\\:saveNewGlobalVar").click();
     
-    $(activeTabPanel() + "globalVarTable__data").shouldHave(text(name), text(value));
-    
-    $(activeTabPanel() + "globalVarTable_\\:0\\:editGlobalVarBtn").click();
-    $("#editGlobalVarModal").shouldBe(visible);
-    
-    $("#editGlobalVarForm\\:editGlobalVarName").shouldBe(exactText(name));
-    $("#editGlobalVarForm\\:editGlobalVarValue").shouldBe(exactValue(value));
+    table.firstColumnShouldBe(size(entryCount + 1));
+    assertEntry(name, value, false);
     
     String newValue = "aNewValue";
-
-    $("#editGlobalVarForm\\:editGlobalVarValue").clear();
-    $("#editGlobalVarForm\\:editGlobalVarValue").sendKeys(newValue);
-    $("#editGlobalVarForm\\:saveGlobalVarConfiguration").click();
+    editVariable(name, newValue, value);
     
-    $(activeTabPanel() + "globalVarTable__data").shouldHave(text(name), text(newValue));
+    assertEntry(name, newValue, false);
+    resetVariable(name);
+    
+    table.firstColumnShouldBe(size(entryCount));
+  }
+
+  private void resetVariable(String name)
+  {
+    variableTable().clickButtonForEntry(name, "resetGlobalVar");
+    $("#resetGlobalVarDialog").shouldBe(visible);
+    $("#resetGlobalVarYesBtn").shouldBe(visible).click();
+  }
+
+  private void editVariable(String name, String value)
+  {
+    editVariable(name, value, null);
+  }
+  
+  private void editVariable(String name, String value, String oldValue)
+  {
+    variableTable().clickButtonForEntry(name, "editGlobalVarBtn");
+    $("#editGlobalVarModal").shouldBe(visible);
+    assertEditVariableDialog(name, oldValue);
+    $("#editGlobalVarForm\\:editGlobalVarValue").clear();
+    $("#editGlobalVarForm\\:editGlobalVarValue").sendKeys(value);
+    $("#editGlobalVarForm\\:saveGlobalVarConfiguration").click();
+  }
+
+  
+  private void assertEditVariableDialog(String name, String oldValue)
+  {
+    if (oldValue == null)
+    {
+      return;
+    }
+    $("#editGlobalVarForm\\:editGlobalVarName").shouldBe(exactText(name));
+    $("#editGlobalVarForm\\:editGlobalVarValue").shouldBe(exactValue(oldValue));
   }
   
   @Test
@@ -72,9 +146,29 @@ public class WebTestVariables
     $("#newGlobalVarModal").shouldNotBe(visible);
   }
   
+  private void assertEntry(String name, String value, boolean isDefault)
+  {
+    var table = variableTable();
+    table.valueForEntryShould(name, 4, exactText(value));
+    if (isDefault)
+    {
+      table.row(name).shouldHave(cssClass("default-value"));
+    }
+    else
+    {
+      table.row(name).shouldNotHave(cssClass("default-value"));
+    }
+    table.buttonForEntryShouldBeDisabled(name, "resetGlobalVar", isDefault);
+  }
+  
+  private Table variableTable()
+  {
+    return new Table(By.id(activeTabPanel() + "globalVarTable"));
+  }
+  
   private String activeTabPanel()
   {
-    return "#apps\\:applicationTabView\\:" + Tab.getSelectedTabIndex() + "\\:form\\:";
+    return "apps:applicationTabView:" + Tab.getSelectedTabIndex() + ":form:";
   }
   
 }
