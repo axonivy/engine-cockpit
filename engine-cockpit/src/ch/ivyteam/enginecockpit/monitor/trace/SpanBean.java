4e7283f3db9207d280c0177f005f0f71432ebfc8
Reto Weiss <reto.weiss@axonivy.com>
2021-12-06 11:28:11 +0100

    XIVY-6668 Add Slow Requests menu and pages that shows traces and spans

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/trace/SpanBean.java
@@ -0,0 +1,98 @@
+package ch.ivyteam.enginecockpit.monitor.trace;
+
+import java.util.stream.Collectors;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+import ch.ivyteam.enginecockpit.commons.TreeView;
+import ch.ivyteam.ivy.trace.Trace;
+import ch.ivyteam.ivy.trace.TraceSpan;
+import ch.ivyteam.ivy.trace.Tracer;
+
+@ManagedBean
+@ViewScoped
+public class SpanBean extends TreeView {
+  private Trace trace;
+  private String traceId;
+
+  public void setTraceId(String traceId) {
+    this.traceId = traceId;
+    reloadTree();
+  }
+
+  public String getTraceId() {
+    return traceId;
+  }
+
+  @Override
+  protected void buildTree() {
+    trace = Tracer.instance().slowTraces().stream().filter(trc -> trc.id().equals(traceId)).findAny().orElse(null);
+    buildTreeNode(trace.rootSpan(), rootTreeNode);
+  }
+
+  private void buildTreeNode(TraceSpan span, TreeNode parentNode) {
+    var node = new DefaultTreeNode(new Span(span), parentNode);
+    span.children().forEach(child -> buildTreeNode(child, node));
+  }
+
+  @Override
+  protected void filterNode(TreeNode node) {
+  }
+
+  public class Span {
+    private TraceSpan span;
+
+    private Span(TraceSpan span) {
+      this.span = span;
+    }
+
+    public String getId() {
+      return span.id();
+    }
+
+    public String getName() {
+      return span.name();
+    }
+
+    public String getStart() {
+      return TraceBean.toLocalTime(span.startEpochNanos());
+    }
+
+    public String getEnd() {
+      return TraceBean.toLocalTime(span.endEpochNanos());
+    }
+
+    public double getExecutionTime() {
+      return TraceBean.toMillis(span.executionTimeNanos());
+    }
+
+    public String getExecutionTimeBackground() {
+      return BackgroundMeterUtil.background(span.executionTimeNanos(), trace.executionTimeNanos());
+    }
+
+    public String getAttributes() {
+      return attributes(", ");
+    }
+
+    public String getAttributesInfo() {
+      return attributes("\n");
+    }
+
+    private String attributes(String delimiter) {
+      return SpanBean.attributes(span, delimiter);
+    }
+  }
+
+  static String attributes(TraceSpan span, String delimiter) {
+    return span
+        .attributes()
+        .stream()
+        .map(attr -> attr.name() + "=" + attr.value())
+        .collect(Collectors.joining(delimiter));
+  }
+
+}
