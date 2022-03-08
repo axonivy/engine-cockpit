561de1167c696ee722b5416187ff74ef9b8a18a7
Reto Weiss <reto.weiss@axonivy.com>
2022-03-08 11:49:13 +0100

    XIVY-6668 Review changes
    - Use Table util class in web tests instead of implementing it in the
    test
    - Use p:staticMessage instead of panelGroup with styleClass
    ui-staticmessage

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestSlowRequests.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestSlowRequests.java
@@ -9,17 +9,17 @@ import static com.codeborne.selenide.Condition.not;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
 import static org.openqa.selenium.By.id;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 
 import ch.ivyteam.enginecockpit.util.Navigation;
+import ch.ivyteam.enginecockpit.util.Table;
 
 @IvyWebTest
 public class WebTestSlowRequests {
@@ -96,31 +96,35 @@ public class WebTestSlowRequests {
   void data_afterRecording() {
     recordData();
 
-    tracesTableEntry("HTTP/1.1 GET", 1).shouldHave(text("HTTP/1.1 GET"));
-    tracesTableEntry("HTTP/1.1 GET", 1).$("a").$("span").shouldHave(attributeMatching("title", "(?s).*http\\.uri.*"));
-    tracesTableEntry("HTTP/1.1 GET", 2).shouldBe(not(empty));
-    tracesTableEntry("HTTP/1.1 GET", 3).shouldBe(not(empty));
-    tracesTableEntry("HTTP/1.1 GET", 4).shouldBe(not(empty));
+    Table traces = new Table(By.id("traces:form:traceTable"), true);
+    traces.tableEntry("HTTP/1.1 GET", 1).shouldHave(text("HTTP/1.1 GET"));
+    traces.tableEntry("HTTP/1.1 GET", 1).$("a").$("span").shouldHave(attributeMatching("title", "(?s).*http\\.uri.*"));
+    traces.tableEntry("HTTP/1.1 GET", 2).shouldBe(not(empty));
+    traces.tableEntry("HTTP/1.1 GET", 3).shouldBe(not(empty));
+    traces.tableEntry("HTTP/1.1 GET", 4).shouldBe(not(empty));
   }
 
   @Test
   void navigateToDetail() {
     recordData();
 
-    tracesTableEntry("HTTP/1.1 GET", 1).$("a").click();
+    Table traces = new Table(By.id("traces:form:traceTable"), true);
+    traces.tableEntry("HTTP/1.1 GET", 1).$("a").click();
     $("h1").shouldHave(text("Slow Request Detail"));
-    spanTableEntry("HTTP/1.1 GET", 1).shouldHave(text("HTTP/1.1 GET"));
-    spanTableEntry("HTTP/1.1 GET", 2).shouldBe(not(empty));
-    spanTableEntry("HTTP/1.1 GET", 3).shouldBe(not(empty));
-    spanTableEntry("HTTP/1.1 GET", 4).shouldBe(not(empty));
-    spanTableEntry("HTTP/1.1 GET", 5).shouldBe(text("http.uri"));
+    Table spans = new Table(By.id("slowRequests:form:tree"), false);
+    spans.tableEntry("HTTP/1.1 GET", 1).shouldHave(text("HTTP/1.1 GET"));
+    spans.tableEntry("HTTP/1.1 GET", 2).shouldBe(not(empty));
+    spans.tableEntry("HTTP/1.1 GET", 3).shouldBe(not(empty));
+    spans.tableEntry("HTTP/1.1 GET", 4).shouldBe(not(empty));
+    spans.tableEntry("HTTP/1.1 GET", 5).shouldBe(text("http.uri"));
   }
 
   @Test
   void data_afterClear() {
     recordData();
     $(id("traces:form:clear")).click();
-    $x(tracesTableBody()).shouldHave(text("No records found."));
+    Table traces = new Table(By.id("traces:form:traceTable"), true);
+    traces.body().shouldHave(text("No records found."));
   }
 
   private static void recordData() {
@@ -138,38 +142,6 @@ public class WebTestSlowRequests {
   private static void start() {
     $(id("traces:form:start")).click();
     $(id("startTraces:start")).click();
-  }
-
-  public SelenideElement tracesTableEntry(String entry, int column) {
-    return $x(findTracesColumnOverEntry(entry) + "/td[" + column + "]");
-  }
-
-  public SelenideElement spanTableEntry(String entry, int column) {
-    return $x(findSpanColumnOverEntry(entry) + "/td[" + column + "]");
-  }
-
-  private String findTracesColumnOverEntry(String entry) {
-    return getTracesFirstColumnElement() + "[text()='" + entry + "']/../../..";
-  }
-
-  private String findSpanColumnOverEntry(String entry) {
-    return getSpanFirstColumnElement() + "[text()='" + entry + "']/../..";
-  }
-
-  private String getTracesFirstColumnElement() {
-    return tracesTableBody()+"/tr/td[1]/a/span";
-  }
-
-  private String getSpanFirstColumnElement() {
-    return spanTableBody()+"/tr/td[1]/span[2]";
-  }
-
-  private String tracesTableBody() {
-    return "//div[@id='traces:form:traceTable']//tbody";
-  }
-
-  private String spanTableBody() {
-    return "//div[@id='slowRequests:form:tree']//tbody";
   }
 
   public static void prepareScreenshot() {
