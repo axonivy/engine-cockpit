74ef2410e75d21d68549d68d889e7632292325e0
Alexander Suter <alexander.suter@axonivy.com>
2022-04-14 09:44:18 +0200

    Fix NPE in RoleDetailBean

--- engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.apache.commons.lang3.StringUtils;
 
+import ch.ivyteam.enginecockpit.commons.ResponseHelper;
 import ch.ivyteam.enginecockpit.security.ldapbrowser.LdapBrowser;
 import ch.ivyteam.enginecockpit.security.model.MemberProperty;
 import ch.ivyteam.enginecockpit.security.model.Role;
@@ -66,7 +67,15 @@ public class RoleDetailBean {
 
   public void setRoleName(String roleName) {
     this.roleName = URLDecoder.decode(roleName, StandardCharsets.UTF_8);
-    var iRole = getSecurityContext().roles().find(this.roleName);
+  }
+
+  public void onload() {
+    var iRole = getSecurityContext().roles().find(roleName);
+    if (iRole == null) {
+      ResponseHelper.notFound("Role '" + roleName + "' not found");
+      return;
+    }
+
     this.role = new Role(iRole);
     var securitySystem = managerBean.getSelectedSecuritySystem();
     this.usersOfRole.setSecuritySystem(securitySystem);
