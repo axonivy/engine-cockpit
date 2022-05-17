c75ee13d471e2a48d6ad4aa23b06ef5b7376e977
Lukas Lieb <lukas.lieb@axonivy.com>
2022-05-17 07:50:20 +0200

    XIVY-8457 Fix System Db init conn test if port is not default
    - Don't delete pmv in test -> flaky and generates huge log

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/application/WebTestPmvDetail.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/application/WebTestPmvDetail.java
@@ -3,25 +3,21 @@ package ch.ivyteam.enginecockpit.application;
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
 import static com.axonivy.ivy.webtest.engine.EngineUrl.DESIGNER;
 import static com.axonivy.ivy.webtest.engine.EngineUrl.isDesigner;
-import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.sleep;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
-import com.codeborne.selenide.Condition;
 
 import ch.ivyteam.enginecockpit.util.Navigation;
 import ch.ivyteam.enginecockpit.util.Table;
@@ -48,12 +44,11 @@ public class WebTestPmvDetail {
 
   @Test
   void pmvDependencies() {
-    dependenciesResolved();
+    dependenciesResolved("ACTIVE");
     Navigation.toApplications();
     deactivatePortalKit();
-    deletePortalKit();
     Navigation.toPmvDetail(APP, PM, PMV);
-    dependenciesNotResolved();
+    dependenciesResolved("INACTIVE");
   }
 
   private void deactivatePortalKit() {
@@ -63,48 +58,25 @@ public class WebTestPmvDetail {
     $(By.id(portalKitVersionId + ":deactivateButton")).shouldBe(visible).click();
   }
 
-  private void deletePortalKit() {
-    var portalKitId = "card:form:tree:" + $$(".activity-name").find(exactText("PortalKit")).parent().parent()
-            .shouldBe(visible).attr("data-rk");
-    sleep(100);
-    $(By.id(portalKitId + ":tasksButton")).shouldBe(visible, enabled).click();
-    $(By.id(portalKitId + ":deleteBtn")).shouldBe(visible, enabled).click();
-    $(By.id("card:form:deleteConfirmYesBtn")).shouldBe(visible, enabled).click();
-  }
-
-  private void dependenciesNotResolved() {
+  private void dependenciesResolved(String portalKitActivityState) {
     Table depTable = new Table(By.id("dependentPmvTable"), true);
     depTable.firstColumnShouldBe(texts("AxonIvyExpress$1", "portal-user-examples$1"));
-    checkPmvEntry(depTable, "AxonIvyExpress$1", "inactive");
-    checkPmvEntry(depTable, "portal-user-examples$1", "inactive");
-
-    Table reqTable = new Table(By.id("requriedPmvTable"), true);
-    reqTable.firstColumnShouldBe(empty);
-
-    Table specTable = new Table(By.id("specifiedTable"));
-    specTable.firstColumnShouldBe(texts("ch.ivyteam.ivy.project.portal:portalKit"));
-    specTable.valueForEntryShould("ch.ivyteam.ivy.project.portal:portalKit", 3, Condition.empty);
-  }
-
-  private void dependenciesResolved() {
-    Table depTable = new Table(By.id("dependentPmvTable"), true);
-    depTable.firstColumnShouldBe(texts("AxonIvyExpress$1", "portal-user-examples$1"));
-    checkPmvEntry(depTable, "AxonIvyExpress$1", "active");
-    checkPmvEntry(depTable, "portal-user-examples$1", "active");
+    checkPmvEntry(depTable, "AxonIvyExpress$1", "ACTIVE");
+    checkPmvEntry(depTable, "portal-user-examples$1", "ACTIVE");
 
     Table reqTable = new Table(By.id("requriedPmvTable"), true);
     reqTable.firstColumnShouldBe(texts("PortalKit$1", "PortalStyle$1"));
-    checkPmvEntry(reqTable, "PortalKit$1", "active");
-    checkPmvEntry(reqTable, "PortalStyle$1", "active");
+    checkPmvEntry(reqTable, "PortalKit$1", portalKitActivityState);
+    checkPmvEntry(reqTable, "PortalStyle$1", "ACTIVE");
 
     Table specTable = new Table(By.id("specifiedTable"));
     specTable.firstColumnShouldBe(texts("ch.ivyteam.ivy.project.portal:portalKit"));
     specTable.valueForEntryShould("ch.ivyteam.ivy.project.portal:portalKit", 3, text("PortalKit$1"));
   }
 
-  private void checkPmvEntry(Table table, String entry, String resolved) {
-    table.tableEntry(entry, 2).find("i").shouldHave(cssClass("activity-state-" + resolved));
-    table.tableEntry(entry, 4).find("i").shouldHave(attribute("title", "ACTIVE"));
+  private void checkPmvEntry(Table table, String entry, String activityState) {
+    table.tableEntry(entry, 2).find("i").shouldHave(cssClass("activity-state-active"));
+    table.tableEntry(entry, 4).find("i").shouldHave(attribute("title", activityState));
     table.tableEntry(entry, 5).find("i").shouldHave(attribute("title", "RELEASED"));
   }
 
