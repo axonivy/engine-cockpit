885c7fa80a862f61a80eacff05c91f9afaec2bc1
Alexander Suter <alexander.suter@axonivy.com>
2022-05-27 15:29:55 +0200

    XIVY-8512 Use second param which is snapshot version to raise the project build plugin

--- raise-build-plugin-version.sh
+++ raise-build-plugin-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn --batch-mode versions:set-property versions:commit -f maven-config/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${1} -DallowSnapshots=true
+mvn --batch-mode versions:set-property versions:commit -f maven-config/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
