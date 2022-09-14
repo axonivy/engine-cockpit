d91d2b99ee8b54ecbd08e12cc35d31d863f86881
Alexander Suter <alexander.suter@axonivy.com>
2022-09-14 08:00:49 +0200

    XIVY-9031 Fix WebTestWizardLicence

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/setup/WebTestWizardLicence.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/setup/WebTestWizardLicence.java
@@ -36,7 +36,7 @@ public class WebTestWizardLicence {
     WebTestWizard.activeStepShouldHaveWarnings();
     uploadLicence();
     $("#uploadStatus").shouldBe(exactText("Success"));
-    $("#licence\\:fileUploadForm\\:licenceInfoTable").shouldHave(text("Lukas Lieb"));
+    $("#licence\\:fileUploadForm\\:licenceInfoTable").shouldHave(text("lukas.lieb@axonivy.com"));
     $("#licence\\:licWarnMessage").shouldHave(empty);
     WebTestWizard.activeStepShouldBeOk();
 
