35d8623efa6bc0bda21d57d71ac45d072beca1a2
Alexander Suter <alexander.suter@axonivy.com>
2022-11-28 14:15:32 +0100

    XIVY-10071 Switch to lazy data model and add a filter for unauthenticated session

--- engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/session/SessionBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/session/SessionBean.java
@@ -1,125 +1,20 @@
 package ch.ivyteam.enginecockpit.monitor.session;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.servlet.http.HttpSession;
-
-import ch.ivyteam.enginecockpit.security.model.SecuritySystem;
-import ch.ivyteam.enginecockpit.security.model.User;
-import ch.ivyteam.ivy.security.ISecurityContext;
-import ch.ivyteam.ivy.security.ISecurityContextRepository;
-import ch.ivyteam.ivy.security.ISessionInternal;
 
 @ManagedBean
 @ViewScoped
 public class SessionBean {
 
-  private List<SessionDto> filteredSessions;
-  private List<SessionDto> sessions = readData();
-  private String filter;
+  private SessionDataModel dataModel = new SessionDataModel();
 
-  public List<SessionDto> getSessions() {
-    return sessions;
-  }
-
-  public List<SessionDto> readData() {
-    return ISecurityContextRepository.instance().all()
-            .stream()
-            .flatMap(s -> s.sessions().all().stream())
-            .filter(s -> !s.isSessionUserSystemUser())
-            .map(s -> new SessionDto((ISessionInternal) s))
-            .collect(Collectors.toList());
-  }
-
-  public List<SessionDto> getFilteredSessions() {
-    return filteredSessions;
-  }
-
-  public void setFilteredSessions(List<SessionDto> filteredSessions) {
-    this.filteredSessions = filteredSessions;
-  }
-
-  public String getFilter() {
-    return filter;
-  }
-
-  public void setFilter(String filter) {
-    this.filter = filter;
+  public SessionDataModel getDataModel() {
+    return dataModel;
   }
 
   public void killSession(SessionDto session) {
     var ses = session.session;
     ses.getSecurityContext().sessions().destroy(ses.getIdentifier(), "ENGINE-COCKPIT");
-    sessions = readData();
-  }
-
-  public static class SessionDto {
-
-    private ISessionInternal session;
-
-    private SessionDto(ISessionInternal session) {
-      this.session = session;
-    }
-
-    public Date getCreatedAt() {
-      return Date.from(session.createdAt());
-    }
-
-    public Date getLastAccessedAt() {
-      return Date.from(session.lastAccessedAt());
-    }
-
-    public String getId() {
-      return String.valueOf(session.getIdentifier());
-    }
-
-    public String getUser() {
-      return session.getSessionUser() == null ? "unauthenticated" : session.getSessionUser().getName();
-    }
-
-    public String getUserLink() {
-      return session.getSessionUser() == null ? "" : new User(session.getSessionUser()).getViewUrl(session.getSecurityContext().getName());
-    }
-
-    public boolean isUserInternal() {
-      return session.isSessionUserSystemUser() || session.isSessionUserUnknown() || isSecuritySystemInternal();
-    }
-
-    public String getAuthMode() {
-      return session.getAuthenticationMode();
-    }
-
-    public String getSecuritySystem() {
-      return session.getSecurityContext().getName();
-    }
-
-    public String getSecuritySystemLink() {
-      return SecuritySystem.link(session.getSecurityContext());
-    }
-
-    public boolean isSecuritySystemInternal() {
-      return ISecurityContext.SYSTEM.equals(session.getSecurityContext().getName());
-    }
-
-    public String getCause() {
-      return session.creationReason();
-    }
-
-    public Set<HttpSession> getHttpSessions() {
-      return session.getHttpSessions();
-    }
-
-    public int getHttpSessionCount() {
-      return session.getHttpSessions().size();
-    }
-
-    public boolean canKill() {
-      return !session.isSessionUserSystemUser();
-    }
   }
 }
---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/session/SessionDto.java
@@ -0,0 +1,97 @@
+package ch.ivyteam.enginecockpit.monitor.session;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivyteam.enginecockpit.security.model.SecuritySystem;
+import ch.ivyteam.enginecockpit.security.model.User;
+import ch.ivyteam.ivy.security.ISecurityContext;
+import ch.ivyteam.ivy.security.ISessionInternal;
+
+public class SessionDto {
+
+  ISessionInternal session;
+
+  private final Date createdAt;
+  private final Date lastAccessedAt;
+  private final String id;
+  private final String user;
+  private final String userLink;
+  private final boolean isUserInternal;
+  private final boolean isSecuritySystemInternal;
+  private final String link;
+  private final String authMode;
+
+  SessionDto(ISessionInternal session) {
+    this.session = session;
+    this.createdAt = Date.from(session.createdAt());
+    this.lastAccessedAt = Date.from(session.lastAccessedAt());
+    this.id = String.valueOf(session.getIdentifier());
+    this.user = session.getSessionUser() == null ? "" : session.getSessionUser().getName();
+    this.userLink = session.getSessionUser() == null ? "" : new User(session.getSessionUser()).getViewUrl(session.getSecurityContext().getName());
+    this.isSecuritySystemInternal = ISecurityContext.SYSTEM.equals(session.getSecurityContext().getName());
+    this.isUserInternal = session.isSessionUserSystemUser() || session.isSessionUserUnknown() || isSecuritySystemInternal;
+    this.link = SecuritySystem.link(session.getSecurityContext());
+    this.authMode = StringUtils.trimToEmpty(session.getAuthenticationMode());
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public Date getLastAccessedAt() {
+    return lastAccessedAt;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getUserLink() {
+    return userLink;
+  }
+
+  public boolean isUserInternal() {
+    return isUserInternal;
+  }
+
+  public String getAuthMode() {
+    return authMode;
+  }
+
+  public String getSecuritySystem() {
+    return session.getSecurityContext().getName();
+  }
+
+  public String getSecuritySystemLink() {
+    return link;
+  }
+
+  public boolean isSecuritySystemInternal() {
+    return isSecuritySystemInternal;
+  }
+
+  public String getCause() {
+    return session.creationReason();
+  }
+
+  public Set<HttpSession> getHttpSessions() {
+    return session.getHttpSessions();
+  }
+
+  public int getHttpSessionCount() {
+    return session.getHttpSessions().size();
+  }
+
+  public boolean canKill() {
+    return !session.isSessionUserSystemUser();
+  }
+}
