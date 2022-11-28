1e906faf241da496e975f76a570f2f9929f36f2e
Alexander Suter <alexander.suter@axonivy.com>
2022-11-28 15:48:07 +0100

    XIVY-10071 Fix sorting of creation time in session monitor

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestSessions.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestSessions.java
@@ -7,6 +7,10 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -49,6 +53,33 @@ class WebTestSessions {
   }
 
   @Test
+  void filterUnauthenticatedSession() {
+    createUnauthenticatedSession();
+
+    Selenide.refresh();
+    new Table(TABLE_ID).firstColumnShouldBe(containExactTextsCaseSensitive(""));
+    $("#form\\:sessionTable\\:filterBtn").click();
+    $("#form\\:sessionTable\\:filterUnauthenticatedSession").click();
+    $("#form\\:sessionTable\\:applyFilter").click();
+    new Table(TABLE_ID).firstColumnShouldBe(containExactTextsCaseSensitive("admin"));
+
+    $("#form\\:sessionTable\\:filterBtn").click();
+    $("#form\\:sessionTable\\:resetFilterBtn").click();
+    new Table(TABLE_ID).firstColumnShouldBe(containExactTextsCaseSensitive(""));
+  }
+
+  private void createUnauthenticatedSession() {
+    try {
+      var url = rootUri().toUrl();
+      var httpClient = HttpClient.newHttpClient();
+      var httpRequest = HttpRequest.newBuilder(URI.create(url)).build();
+      httpClient.send(httpRequest, BodyHandlers.discarding());
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @Test
   void liveStats() {
     EngineCockpitUtil.assertLiveStats(List.of("Sessions"));
   }
@@ -56,25 +87,35 @@ class WebTestSessions {
   @Test
   void killSession() {
     openAnotherSession();
-    table.search(SESSION_USER);
     table.firstColumnShouldBe(textsInAnyOrder(SESSION_USER));
     table.clickButtonForEntry(SESSION_USER, "killSession");
+    table.search(SESSION_USER);
     table.firstColumnShouldBe(CollectionCondition.empty);
   }
 
   private void openAnotherSession() {
     $(".layout-topbar-actions .help-link a").shouldBe(visible).click();
     Selenide.switchTo().window(1);
-    if (EngineUrl.isDesigner()) {
-      Selenide.open(EngineUrl.create().app("default-workflow").path("faces/login.xhtml").toUrl());
-    } else {
-      Selenide.open(EngineUrl.create().app("test").path("login").toUrl());
-    }
+    openLogin();
     $("#loginForm\\:userName").shouldBe(visible).sendKeys(SESSION_USER);
     $("#loginForm\\:password").shouldBe(visible).sendKeys(SESSION_USER);
     $("#loginForm\\:login").click();
     $("#sessionUserName").shouldHave(text(SESSION_USER));
     Selenide.switchTo().window(0);
     Selenide.refresh();
+  }
+
+  private void openLogin() {
+    var url = rootUri()
+
+            .toUrl();
+    Selenide.open(url);
+  }
+
+  private EngineUrl rootUri() {
+    if (EngineUrl.isDesigner()) {
+      return EngineUrl.create().app("default-workflow").path("faces/login.xhtml");
+    }
+    return EngineUrl.create().app("test").path("login");
   }
 }
