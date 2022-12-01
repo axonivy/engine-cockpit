b5da18bd8d19e750e3dad56115d0ae3a95e7fbad
Lukas Lieb <lukas.lieb@axonivy.com>
2022-12-01 15:24:34 +0100

    XIVY-10122 Add branding download btn

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/download/AllResourcesDownload.java
@@ -0,0 +1,7 @@
+package ch.ivyteam.enginecockpit.download;
+
+import org.primefaces.model.StreamedContent;
+
+public interface AllResourcesDownload {
+  StreamedContent getAllResourcesDownload();
+}
