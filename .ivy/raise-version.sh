7194f8386eccf04598a40780a325225403d7200d
Alexander Suter <alexander.suter@axonivy.com>
2022-12-15 12:07:18 +0100

    Use maven versions plugin 2.13.0, because 2.14.0 is broken

--- .ivy/raise-version.sh
+++ .ivy/raise-version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn -B versions:set -DnewVersion=${1} -DprocessAllModules
-mvn -B versions:commit -DprocessAllModules
+mvn -B org.codehaus.mojo:versions-maven-plugin:2.13.0:set -DnewVersion=${1} -DprocessAllModules
+mvn -B org.codehaus.mojo:versions-maven-plugin:2.13.0:commit -DprocessAllModules
