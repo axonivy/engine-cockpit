6960ad91c0bb77229e302c9e200fcdbaa173e2cd
Alexander Suter <alexander.suter@axonivy.com>
2023-03-09 15:40:49 +0100

    XIVY-9172 Add test

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestSecurityIdentityProviderAzure.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestSecurityIdentityProviderAzure.java
@@ -12,11 +12,14 @@ import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.primeui.PrimeUi;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 
 import ch.ivyteam.enginecockpit.util.Navigation;
+import ch.ivyteam.enginecockpit.util.Table;
 
-@IvyWebTest
+@IvyWebTest(headless = false)
 class WebTestSecurityIdentityProviderAzure {
 
   private final static String NAME = "test-azure";
@@ -27,11 +30,11 @@ class WebTestSecurityIdentityProviderAzure {
     Navigation.toSecuritySystem();
     createSecuritySystem("Azure Active Directory", NAME);
     Navigation.toSecuritySystemProvider(NAME);
-
     stringProperty();
     Selenide.refresh();
     passwordProperty();
-
+    Selenide.refresh();
+    keyValueProperty();
     WebTestSecuritySystem.deleteSecuritySystem(NAME);
   }
 
@@ -61,6 +64,50 @@ class WebTestSecurityIdentityProviderAzure {
     property.shouldHave(attribute("placeholder", "************"));
   }
 
+  private void keyValueProperty() {
+    var table = new Table(By.id("securityIdentityProviderForm:group:1:property:0:keyValueTable"));
+    table.firstColumnShouldBe(CollectionCondition.empty);
+
+    // add
+    $("#securityIdentityProviderForm\\:group\\:1\\:newPropertyKeyValue")
+            .shouldBe(visible)
+            .click();
+    $("#identityProviderKeyValueForm\\:attributeNameInput")
+            .should(visible)
+            .sendKeys("user property");
+    $("#identityProviderKeyValueForm\\:attributeValueInput")
+            .should(visible)
+            .sendKeys("azure property");
+    $("#identityProviderKeyValueForm\\:savePropertyKeyAttribute")
+            .should(visible)
+            .click();
+    success();
+    table.columnShouldBe(1, CollectionCondition.exactTexts("user property"));
+    table.columnShouldBe(2, CollectionCondition.exactTexts("azure property"));
+
+    // edit
+    $("#securityIdentityProviderForm\\:group\\:1\\:property\\:0\\:keyValueTable\\:0\\:editPropertyBtn")
+            .should(visible)
+            .click();
+    $("#identityProviderKeyValueForm\\:attributeNameInput").should(Condition.disabled);
+    var p = $("#identityProviderKeyValueForm\\:attributeValueInput")
+      .should(visible);
+    p.clear();
+    p.sendKeys("changed azure property");
+    $("#identityProviderKeyValueForm\\:savePropertyKeyAttribute")
+      .should(visible)
+      .click();
+    success();
+    table.columnShouldBe(1, CollectionCondition.exactTexts("user property"));
+    table.columnShouldBe(2, CollectionCondition.exactTexts("changed azure property"));
+
+    // delete
+    $("#securityIdentityProviderForm\\:group\\:1\\:property\\:0\\:keyValueTable\\:0\\:deleteKeyValueBtn")
+      .click();
+    success();
+    table.firstColumnShouldBe(CollectionCondition.empty);
+  }
+
   private void createSecuritySystem(String providerName, String securitySystemName) {
     $("#form\\:createSecuritySystemBtn").click();
     $("#newSecuritySystemForm\\:newSecuritySystemNameInput").sendKeys(securitySystemName);
@@ -70,10 +117,10 @@ class WebTestSecurityIdentityProviderAzure {
   }
 
   private void save() {
-    $("#securityIdentityProviderForm\\:save").shouldBe(visible).click();
+    $("#securityIdentityProviderForm\\:group\\:0\\:save").shouldBe(visible).click();
   }
 
   private void success() {
-    $("#securityIdentityProviderForm\\:securityIdentityProviderSaveSuccess_container").shouldHave(text("Successfully saved 'Azure Active Directory'"));
+    $("#securityIdentityProviderForm\\:securityIdentityProviderSaveSuccess_container").shouldHave(text("Successfully saved"));
   }
 }
