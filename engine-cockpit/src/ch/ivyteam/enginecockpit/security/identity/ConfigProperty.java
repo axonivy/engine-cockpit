c0cc2235cbab8982837f4e430a7c92e0e55dd29b
Alexander Suter <alexander.suter@axonivy.com>
2023-03-09 16:32:46 +0100

    XIVY-9172 Provide a generic view to make key-value list of an identity provider editable

--- engine-cockpit/src/ch/ivyteam/enginecockpit/security/identity/ConfigProperty.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/identity/ConfigProperty.java
@@ -1,23 +1,52 @@
 package ch.ivyteam.enginecockpit.security.identity;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.apache.commons.lang3.StringUtils;
 
 import ch.ivyteam.ivy.configuration.meta.Metadata;
+import ch.ivyteam.ivy.security.external.SecuritySystemConfig;
 
 public class ConfigProperty {
 
+  private final SecuritySystemConfig cfg;
   private final String key;
   private String value;
   private final Metadata metadata;
+  private final String label;
 
-  public ConfigProperty(String key, String value, Metadata metadata) {
+  // encapsulate the special handling for a key value property
+  private final KeyValueProperty keyValueProperty;
+
+  public ConfigProperty(SecuritySystemConfig cfg, String key, String value, Map<String, String> keyValue, Metadata metadata) {
+    this.cfg = cfg;
     this.key = key;
     this.value = value;
     this.metadata = metadata;
+    this.label = toLabel();
+    this.keyValueProperty = new KeyValueProperty(keyValue);
   }
 
   public String getName() {
     return key;
+  }
+
+  private String toLabel() {
+    if (isKeyValue()) {
+      return Arrays.stream(key.split(".")).collect(Collectors.joining(" "));
+    }
+    if (key.contains(".")) {
+      return StringUtils.substringAfter(key, ".");
+    }
+    return key;
+  }
+
+  public String getLabel() {
+    return label;
   }
 
   public String getValue() {
@@ -39,15 +68,73 @@ public class ConfigProperty {
     return metadata.description();
   }
 
+  public boolean isString() {
+    return !isKeyValue() && !isPassword();
+  }
+
   public boolean isPassword() {
     return metadata.isPassword();
+  }
+
+  public boolean isKeyValue() {
+    return metadata.isKeyValue();
   }
 
   public String getPasswordPlaceholder() {
     return "*".repeat(value.length());
   }
 
-  public boolean isString() {
-    return !isPassword();
+  public KeyValueProperty getKeyValueProperty() {
+    return keyValueProperty;
+  }
+
+  @SuppressWarnings("restriction")
+  public class KeyValueProperty {
+
+    private final Map<String, String> keyValue;
+    private final ch.ivyteam.ivy.configuration.restricted.meta.KeyValueVarType type;
+
+    private KeyValueProperty(Map<String, String> keyValue) {
+      this.keyValue = keyValue;
+      this.type = toType();
+    }
+
+    private ch.ivyteam.ivy.configuration.restricted.meta.KeyValueVarType toType() {
+      if (metadata.type() instanceof ch.ivyteam.ivy.configuration.restricted.meta.KeyValueVarType t) {
+        return t;
+      }
+      return null;
+    }
+
+    public Set<Entry<String, String>> getKeyValue() {
+      return keyValue.entrySet();
+    }
+
+    public Map<String, String> keyValue() {
+      return keyValue;
+    }
+
+    public void addKeyValue(String k, String v) {
+      keyValue.put(k, v);
+      save();
+    }
+
+    public void removeKeyValue(String k) {
+      keyValue.remove(k);
+      save();
+    }
+
+    private void save() {
+      cfg.setProperty(getName(), keyValue());
+      IdentityProviderBean.message();
+    }
+
+    public String getKeyValueKeyName() {
+      return type.keyName();
+    }
+
+    public String getKeyValueValueName() {
+      return type.valueName();
+    }
   }
 }
---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/identity/ConfigPropertyGroup.java
@@ -0,0 +1,77 @@
+package ch.ivyteam.enginecockpit.security.identity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ch.ivyteam.enginecockpit.security.identity.ConfigProperty.KeyValueProperty;
+
+public class ConfigPropertyGroup {
+
+  private final String name;
+  private final List<ConfigProperty> properties = new ArrayList<>();
+
+  public ConfigPropertyGroup(String name) {
+    this.name = name;
+  }
+
+  public void add(ConfigProperty property) {
+    properties.add(property);
+  }
+
+  public List<ConfigProperty> getProperties() {
+    return properties;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public boolean isKeyValue() {
+    return properties.get(0).isKeyValue();
+  }
+
+  public KeyValueProperty getKeyValueProperty() {
+    return properties.get(0).getKeyValueProperty();
+  }
+
+  public static List<ConfigPropertyGroup> toGroups(List<ConfigProperty> properties) {
+    var groups = new ArrayList<ConfigPropertyGroup>();
+    for (var p : properties) {
+      var propertyName = p.getName();
+      if (isParentKey(properties, propertyName)) {
+        continue;
+      }
+      var name = toName(p);
+      var group = groups.stream()
+              .filter(g -> g.getName().equals(name))
+              .findAny()
+              .orElseGet(() -> { var g = new ConfigPropertyGroup(name); groups.add(g); return g; });
+      group.add(p);
+    }
+    return groups;
+  }
+
+  private static boolean isParentKey(List<ConfigProperty> properties, String n) {
+    return properties.stream()
+            .map(ConfigProperty::getName)
+            .anyMatch(na -> !n.equals(na) && na.startsWith(n));
+  }
+
+  private static String toName(ConfigProperty p) {
+    var name = p.getName();
+    if (p.isKeyValue()) {
+      return Arrays.stream(StringUtils.splitByCharacterTypeCamelCase(name))
+              .map(part -> StringUtils.remove(part, "."))
+              .map(part -> part.trim())
+              .collect(Collectors.joining(" "));
+    }
+    if (name.contains(".")) {
+      return StringUtils.substringBefore(name, ".");
+    }
+    return "Configuration";
+  }
+}
