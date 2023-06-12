803d5e855c539381b466492aa21b01ffe0601361
Jacek Lajdecki <jacek.lajdecki@axonivy.com>
2023-06-12 17:33:03 +0200

    XIVY-11599 Intermediate events

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/events/intermediate/IntermediateEvent.java
@@ -0,0 +1,69 @@
+package ch.ivyteam.enginecockpit.monitor.events.intermediate;
+
+import javax.management.ObjectName;
+
+import ch.ivyteam.enginecockpit.monitor.events.Event;
+
+public final class IntermediateEvent extends Event {
+
+  public IntermediateEvent(ObjectName name) {
+    super(name);
+  }
+
+  @Override
+  public String getFullRequestPath() {
+    return getApplication() +
+            "/" +
+            getPm() +
+            "$" +
+            getPmv() +
+            "/" +
+            getProcessElementId();
+  }
+
+  public String getLastFiringTimestamp() {
+    return getDateAttribute("lastFiringTimestamp");
+  }
+
+  public String getProcessElementId() {
+    return readStringAttribute("processElementId");
+  }
+
+  @Override
+  public long getExecutions() {
+    return readLongAttribute("firings");
+  }
+
+  @Override
+  public long getErrors() {
+    return readLongAttribute("firingErrors");
+  }
+
+  public String getMinExecutionTime() {
+    long executions = getExecutions();
+    if (executions == 0) {
+      return NOT_AVAILABLE;
+    }
+    return formatMicros((Long) readAttribute("pollsMinExecutionTimeInMicroSeconds"));
+  }
+
+  public String getAvgExecutionTime() {
+    var total = (Long) readAttribute("pollsTotalExecutionTimeInMicroSeconds");
+    if (total == null) {
+      return formatMicros(total);
+    }
+    long executions = getExecutions();
+    if (executions == 0) {
+      return NOT_AVAILABLE;
+    }
+    return formatMicros(total / executions);
+  }
+
+  public String getMaxExecutionTime() {
+    long executions = getExecutions();
+    if (executions == 0) {
+      return NOT_AVAILABLE;
+    }
+    return formatMicros((Long) readAttribute("pollsMaxExecutionTimeInMicroSeconds"));
+  }
+}
---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/events/start/StartEventBean.java
@@ -0,0 +1,66 @@
+package ch.ivyteam.enginecockpit.monitor.events.start;
+
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+
+@ManagedBean
+@ViewScoped
+public class StartEventBean {
+
+  private List<StartEvent> beans;
+  private List<StartEvent> filteredBeans;
+  private String filter;
+  private StartEvent selected;
+
+  public StartEventBean() {
+    refresh();
+  }
+
+  public void refresh() {
+    try {
+      beans = ManagementFactory.getPlatformMBeanServer()
+              .queryNames(new ObjectName("ivy Engine:type=Process Start Event Bean,application=*,pm=*,pmv=*,name=*"), null)
+              .stream()
+              .map(StartEvent::new)
+              .toList();
+      beans = new ArrayList<>(beans);
+    } catch (MalformedObjectNameException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  public List<StartEvent> getBeans() {
+    return beans;
+  }
+
+  public List<StartEvent> getFilteredBeans() {
+    return filteredBeans;
+  }
+
+  public void setFilteredBeans(List<StartEvent> filteredBeans) {
+    this.filteredBeans = filteredBeans;
+  }
+
+  public String getFilter() {
+    return filter;
+  }
+
+  public void setFilter(String filter) {
+    this.filter = filter;
+  }
+
+  public StartEvent getSelected() {
+    return selected;
+  }
+
+  public void setSelected(StartEvent selected) {
+    this.selected = selected;
+  }
+
+}
