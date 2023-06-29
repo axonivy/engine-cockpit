e48b438fbd30e9363de56ea587acee9ce3b3d19d
Reto Weiss <reto.weiss@axonivy.com>
2023-06-29 09:33:28 +0200

    XIVY-11623 Add threads to intermediate events too.

--- engine-cockpit-test-data/src/ch/ivyteam/enginecockpit/monitor/PollErrorIntermediateClass.java
+++ engine-cockpit-test-data/src/ch/ivyteam/enginecockpit/monitor/PollErrorIntermediateClass.java
@@ -1,6 +1,7 @@
 package ch.ivyteam.enginecockpit.monitor;
 
 import ch.ivyteam.ivy.process.intermediateevent.AbstractProcessIntermediateEventBean;
+import ch.ivyteam.ivy.process.intermediateevent.IProcessIntermediateEventBeanRuntime;
 
 public class PollErrorIntermediateClass extends AbstractProcessIntermediateEventBean {
 
@@ -8,6 +9,12 @@ public class PollErrorIntermediateClass extends AbstractProcessIntermediateEvent
     super("PollErrorIntermediateClass", "This IntermediateClass throws an error in poll method", String.class);
   }
 
+  @Override
+  public void initialize(IProcessIntermediateEventBeanRuntime eventRuntime, String configuration) {
+    super.initialize(eventRuntime, configuration);
+    eventRuntime.threads().boundToEventLifecycle(() -> {});
+    
+  }
   @Override
   public void poll() {
     throw new RuntimeException("Exception in Poll Method");
