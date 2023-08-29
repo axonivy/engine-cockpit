3f6c60c0226bcd2e621bb6070e4e39cab19cfec9
Alexander Suter <alexander.suter@axonivy.com>
2023-08-29 16:33:59 +0200

    XIVY-10173 Bring S3 Blob Storage to Life

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/blob/BlobDto.java
@@ -0,0 +1,82 @@
+package ch.ivyteam.enginecockpit.monitor.blob;
+
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+import ch.ivyteam.enginecockpit.security.model.User;
+import ch.ivyteam.ivy.blob.storage.core.Blob;
+import ch.ivyteam.ivy.blob.storage.core.BlobService;
+import ch.ivyteam.util.mime.MimeTypeEvaluator;
+
+public class BlobDto {
+
+  private final Blob blob;
+
+  private final Date createdAt;
+  private final String creatorLink;
+
+  private final Date modifiedAt;
+  private final String modifierLink;
+
+  public BlobDto(Blob blob) {
+    this.blob = blob;
+    this.createdAt = Date.from(blob.createdAt());
+    this.modifiedAt = Date.from(blob.modifiedAt());
+    this.creatorLink = new User(blob.creator()).getViewUrl(blob.creator().getSecurityContext().getName());
+    this.modifierLink = new User(blob.modifier()).getViewUrl(blob.modifier().getSecurityContext().getName());
+  }
+
+  public String getId() {
+    return blob.uuid();
+  }
+
+  public String getPath() {
+    if (BlobService.of(blob.app()).isUsingLegacyPath()) {
+      return blob.filePath();
+    }
+    return blob.path();
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public String getCreator() {
+    return blob.creator().getName();
+  }
+
+  public String getCreatorLink() {
+    return creatorLink;
+  }
+
+  public Date getModifiedAt() {
+    return modifiedAt;
+  }
+
+  public String getModifier() {
+    return blob.modifier().getName();
+  }
+
+  public String getModifierLink() {
+    return modifierLink;
+  }
+
+  public String getSize() {
+    return FileUtils.byteCountToDisplaySize(blob.size());
+  }
+
+  public String getMimeType() {
+    return MimeTypeEvaluator.ofFilename(blob.fileName());
+  }
+
+  public StreamedContent download() {
+    return DefaultStreamedContent.builder()
+            .name(blob.fileName())
+            .contentType(getMimeType())
+            .stream(() -> BlobService.of(blob.app()).read(blob.uuid()))
+            .build();
+  }
+}
---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/monitor/blob/BlobsDataModel.java
@@ -0,0 +1,100 @@
+package ch.ivyteam.enginecockpit.monitor.blob;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
+import org.primefaces.model.SortOrder;
+
+import ch.ivyteam.enginecockpit.security.model.SecuritySystem;
+import ch.ivyteam.ivy.blob.storage.core.impl.BlobRepository;
+import ch.ivyteam.ivy.blob.storage.core.query.BlobQuery;
+import ch.ivyteam.ivy.jsf.primefaces.sort.SortMetaConverter;
+
+public class BlobsDataModel extends LazyDataModel<BlobDto> {
+
+  private SecuritySystem securitySystem;
+  private String filter;
+
+  public BlobsDataModel(SecuritySystem securitySystem) {
+    this.securitySystem = securitySystem;
+  }
+
+  public void setFilter(String filter) {
+    this.filter = filter;
+  }
+
+  public String getFilter() {
+    return filter;
+  }
+
+  @Override
+  public List<BlobDto> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+    var query = query();
+
+    applyFilter(query);
+    applyOrdering(query, sortBy);
+
+    var blobs = query
+            .executor()
+            .results(first, pageSize).stream()
+            .map(BlobDto::new)
+            .collect(Collectors.toList());
+    setRowCount((int) query.executor().count());
+    return blobs;
+  }
+
+  private void applyFilter(BlobQuery query) {
+    if (StringUtils.isNotEmpty(filter)) {
+      var dbFilter = "%" + filter + "%";
+      query.where().and(query().where()
+              .path().isLikeIgnoreCase(dbFilter))
+            .or()
+              .uUID().isEqual(dbFilter);
+    }
+  }
+
+  private BlobQuery query() {
+    return BlobRepository.of(securitySystem.getSecurityContext()).query();
+  }
+
+  private static void applyOrdering(BlobQuery query, Map<String, SortMeta> sortBy) {
+    var sort = new SortMetaConverter(sortBy);
+    var sortOrder = sort.toOrder();
+    var sortField = sort.toField();
+
+    if (StringUtils.isEmpty(sortField)) {
+      return;
+    }
+    if ("createdAt".equals(sortField)) {
+      applyOrdering(query.orderBy().createdAt(), sortOrder);
+    }
+    if ("modifiedAt".equals(sortField)) {
+      applyOrdering(query.orderBy().modifiedAt(), sortOrder);
+    }
+    if ("path".equals(sortField)) {
+      applyOrdering(query.orderBy().path(), sortOrder);
+    }
+    if ("size".equals(sortField)) {
+      applyOrdering(query.orderBy().size(), sortOrder);
+    }
+  }
+
+  private static void applyOrdering(BlobQuery.OrderByColumnQuery query, SortOrder sortOrder) {
+    if (SortOrder.ASCENDING == sortOrder) {
+      query.ascending();
+    }
+    if (SortOrder.DESCENDING == sortOrder) {
+      query.descending();
+    }
+  }
+
+  @Override
+  public int count(Map<String, FilterMeta> filterBy) {
+    return getRowCount();
+  }
+}
