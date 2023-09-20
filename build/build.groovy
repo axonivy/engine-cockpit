5694cea6be92f755ace25b76f12092499aad6f2c
Lukas Lieb <lukas.lieb@axonivy.com>
2023-09-20 12:50:02 +0200

    Indroduce integration build pipeline

--- build/build.groovy
+++ build/build.groovy
@@ -1,4 +1,4 @@
-def build(def mvnArgs = '') {
+def buildIntegration(def mvnArgs = '') {
   withCredentials([string(credentialsId: 'github.ivy-team.token', variable: 'GITHUB_TOKEN')]) {
     def random = (new Random()).nextInt(10000000)
     def networkName = "build-" + random
@@ -10,23 +10,15 @@ def build(def mvnArgs = '') {
       docker.image('mysql:5').withRun("-e \"MYSQL_ROOT_PASSWORD=1234\" -e \"MYSQL_DATABASE=test\" --name ${dbName} --network ${networkName}") {
         docker.image("selenium/standalone-firefox:4.10").withRun("-e START_XVFB=false --shm-size=2g --name ${seleniumName} --network ${networkName} --shm-size 1g --hostname=ivy") {
           docker.build('maven', '-f build/Dockerfile .').inside("--name ${ivyName} --network ${networkName}") {
-            def phase = isReleaseOrMasterBranch() ? 'deploy' : 'verify'
-            maven cmd: "clean ${phase} -ntp " +
-                "-Divy.engine.version='[10.0.0,]' " +
-                "-Dwdm.gitHubTokenName=ivy-team " +
+            def mvnBuildArgs = "-Dwdm.gitHubTokenName=ivy-team " +
                 "-Dwdm.gitHubTokenSecret=${env.GITHUB_TOKEN} " +
                 "-Dengine.page.url=${params.engineSource} " +
                 "-Dtest.engine.url=http://${ivyName}:8080 " +
                 "-Dselenide.remote=http://${seleniumName}:4444/wd/hub " +
                 "-Ddb.host=${dbName} " + 
-                "-Dmaven.test.skip=false " +
                 mvnArgs
+            mvnBuild(mvnBuildArgs);
 
-            recordIssues tools: [mavenConsole()], unstableTotalAll: 1, filters: [
-              excludeMessage('The system property test.engine.url is configured twice!*'),
-              excludeMessage('JAR will be empty*')
-            ]
-            junit testDataPublishers: [[$class: 'AttachmentPublisher'], [$class: 'StabilityTestDataPublisher']], testResults: '**/target/surefire-reports/**/*.xml'
             archiveArtifacts '.ivy-engine/logs/*'
             archiveArtifacts artifacts: '**/target/selenide/reports/**/*', allowEmptyArchive: true
           }
@@ -36,6 +28,23 @@ def build(def mvnArgs = '') {
       sh "docker network rm ${networkName}"
     }
   }
+}
+
+def build() {
+  docker.build('maven', '-f build/Dockerfile .').inside("") {
+    mvnBuild("-Pcockpit ");
+  }
+}
+
+def mvnBuild(def mvnArgs = '') {
+  def phase = isReleaseOrMasterBranch() ? 'deploy' : 'verify'
+  maven cmd: "clean ${phase} -ntp -Divy.engine.version='[10.0.0,]' -Dmaven.test.skip=false " + mvnArgs
+  
+  recordIssues tools: [mavenConsole()], unstableTotalAll: 1, filters: [
+    excludeMessage('The system property test.engine.url is configured twice!*'),
+    excludeMessage('JAR will be empty*')
+  ]
+  junit testDataPublishers: [[$class: 'AttachmentPublisher'], [$class: 'StabilityTestDataPublisher']], testResults: '**/target/surefire-reports/**/*.xml'
 }
 
 def isReleaseOrMasterBranch() {
