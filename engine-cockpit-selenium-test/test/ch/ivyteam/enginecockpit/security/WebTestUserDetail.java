12b3539d137001b5848505e01d5742d1e541e7ec
Lukas Giger <lukas.giger@axonivy.com>
2023-09-25 16:07:46 +0200

    Revert "XIVY-12212 fix WebTestUserDetail.substitutes"
    This reverts commit 07bfdf9eaa98d6e5c6c8e04aeaab0d41089f6b3d.

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestUserDetail.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/security/WebTestUserDetail.java
@@ -285,10 +285,10 @@ class WebTestUserDetail {
     Table tableWithoutLink = new Table(By.id(tableId));
 
     tableWithLink.firstColumnShouldBe(size(3));
-    tableWithLink.firstColumnShouldBe(CollectionCondition.exactTexts("bar", "jon", "bar"));
+    tableWithLink.firstColumnShouldBe(CollectionCondition.exactTexts("substitute1", "substitute2", "substitute3"));
     tableWithoutLink.columnShouldBe(2, CollectionCondition.exactTexts("On absence", "Permanent", "On absence"));
     tableWithoutLink.columnShouldBe(3, CollectionCondition.exactTexts("Personal", "Personal", "Role"));
-    tableWithLink.columnShouldBe(4, CollectionCondition.exactTexts("", "", "worker"));
+    tableWithLink.columnShouldBe(4, CollectionCondition.exactTexts("", "", "role"));
 
     EngineCockpitUtil.cleanupSubstitutes();
   }
