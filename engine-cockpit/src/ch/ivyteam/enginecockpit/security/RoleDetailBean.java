c2d30d0ce1a49a5eab7a6ee16380b7102ace3a03
Alexander Suter <alexander.suter@axonivy.com>
2023-11-02 10:21:53 +0100

    No longer allow to delete top level role

--- engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
@@ -197,6 +197,10 @@ public class RoleDetailBean {
     return !isTopLevelRole();
   }
 
+  public boolean canDeleteRole() {
+    return !isTopLevelRole();
+  }
+
   private boolean isTopLevelRole() {
     return getIRole().getParent() == null;
   }
