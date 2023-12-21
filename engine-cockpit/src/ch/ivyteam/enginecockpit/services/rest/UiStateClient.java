63efd3ec17d25c4476436610bd70240c5ddc8ba9
Tobias Gretler <tobias.gretler@axonivy.com>
2023-12-21 15:23:32 +0100

    XIVY-12264 fix: UI endlessly blocked due to connection problem

--- engine-cockpit/src/ch/ivyteam/enginecockpit/services/rest/UiStateClient.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/services/rest/UiStateClient.java
@@ -1,21 +1,36 @@
 package ch.ivyteam.enginecockpit.services.rest;
 
+import java.util.concurrent.TimeUnit;
+
 import ch.ivyteam.enginecockpit.services.model.RestClientDto;
 import ch.ivyteam.ivy.rest.client.RestClient;
+import ch.ivyteam.ivy.rest.client.RestClient.Builder;
 
 public class UiStateClient {
 
-  private static final String REST_PROP_USERNAME = "username";
-  private static final String REST_PROP_PASSWORD = "password";
+  private final Builder builder;
 
-  public static RestClient toUiStateClient(RestClient original, RestClientDto dto) {
-    var restBuilder = original.toBuilder()
-            .uri(dto.getConnectionUrl())
-            .property(REST_PROP_USERNAME, dto.getUsername());
+  public UiStateClient(RestClient original) {
+    builder = original.toBuilder();
+  }
+
+  @SuppressWarnings("restriction")
+  public UiStateClient setUiState(RestClientDto dto) {
+    builder.uri(dto.getConnectionUrl())
+      .property(ch.ivyteam.ivy.rest.client.config.RestClientProperty.Authentication.USERNAME, dto.getUsername());
     if (dto.passwordChanged()) {
-      restBuilder.property(REST_PROP_PASSWORD, dto.getPassword());
+      builder.property(ch.ivyteam.ivy.rest.client.config.RestClientProperty.Authentication.PASSWORD, dto.getPassword());
     }
-    return restBuilder.toRestClient();
+    return this;
+  }
+
+  public UiStateClient setTimeout(TimeUnit unit, int amount) {
+    builder.property("jersey.config.client.connectTimeout", String.valueOf(unit.toMillis(amount)));
+    return this;
+  }
+
+  public RestClient toClient() {
+    return builder.toRestClient();
   }
 
 }
