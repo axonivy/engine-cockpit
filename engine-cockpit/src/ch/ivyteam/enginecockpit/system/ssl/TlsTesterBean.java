fdcb6760ed8c94a3e781258921a249ae1d0ed721
Tobias Gretler <tobias.gretler@axonivy.com>
2024-02-07 13:40:41 +0100

    wip

--- engine-cockpit/src/ch/ivyteam/enginecockpit/system/ssl/TlsTesterBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/system/ssl/TlsTesterBean.java
@@ -1,10 +1,15 @@
 package ch.ivyteam.enginecockpit.system.ssl;
 
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import ch.ivyteam.ivy.ssl.restricted.tester.TLSTest;
 import ch.ivyteam.ivy.ssl.restricted.tester.TLSTestData;
@@ -24,12 +29,39 @@ public class TlsTesterBean {
     return Uri.startsWith("ldaps");
   }
 
+  public void fileDownloadView() {
+    TLSTest store = new TLSTest(testResult, "https://test-webservices.ivyteam.io:8090/api/v3");
+    var certs = store.getMissingCerts();
+    var aliases = store.getAliase();
+    var alias = aliases.get(2);
+    var certificate = certs.get(2);
+    try {
+        ByteArrayInputStream stream = new ByteArrayInputStream(certificate.getEncoded());
+
+        file = DefaultStreamedContent.builder()
+                .name(alias + ".crt")
+                .contentType("application/x-x509-ca-cert")
+                .stream(() -> stream)
+                .build();
+
+    } catch (CertificateEncodingException e) {
+        e.printStackTrace();
+    }
+  }
+
+  private StreamedContent file;
+
+  public StreamedContent getFile() {
+      return file;
+  }
+
   public void testConnection(String targetUri) {
     testResult.clear();
     setTlsTestRendered(true);
     TLSTest test = new TLSTest(testResult, targetUri);
     test.runTLSTests();
   }
+
   public List<TLSTestData> getTestResult() {
     return testResult;
   }
