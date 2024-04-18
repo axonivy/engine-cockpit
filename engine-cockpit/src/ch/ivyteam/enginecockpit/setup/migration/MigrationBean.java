cf00b03733316c4873158269b402de1e5cf6e6e1
Alexander Suter <alexander.suter@axonivy.com>
2024-04-18 10:42:43 +0200

    XIVY-13760 Add UI to decide if configuration files should be written to tmp dir

--- engine-cockpit/src/ch/ivyteam/enginecockpit/setup/migration/MigrationBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/setup/migration/MigrationBean.java
@@ -1,5 +1,6 @@
 package ch.ivyteam.enginecockpit.setup.migration;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -25,12 +26,13 @@ public class MigrationBean {
   private CompletableFuture<String> migrationRunner;
   private MigrationRunner client;
   private Result result;
+  private boolean writeToTmp = false;
 
   public void checkOldEngineLocation() {
     try {
       migrator = new EngineMigrator(pathToOldEngine);
       result = migrator.check();
-      tasks = result.tasks().stream().map(Task::new).collect(Collectors.toList());
+      tasks = loadTasks(result);
       client = new MigrationRunner(tasks);
       running = MigrationState.START;
     } catch (Exception ex) {
@@ -42,11 +44,36 @@ public class MigrationBean {
     }
   }
 
+  public boolean isInPlace() {
+    if (result == null) {
+      return false;
+    }
+    if (!result.success()) {
+      return false;
+    }
+    return result.mode() == EngineMigrator.Mode.IN_PLACE;
+  }
+
+  public boolean isWriteToTmp() {
+    return writeToTmp;
+  }
+
+  public void setWriteToTmp(boolean writeToTmp) {
+    this.writeToTmp = writeToTmp;
+  }
+
+  private static List<Task> loadTasks(Result result) {
+    if (result.success()) {
+      return result.tasks().stream().map(Task::new).collect(Collectors.toList());
+    }
+    return new ArrayList<>();
+  }
+
   public void execute() throws Exception {
     running = MigrationState.RUNNING;
     migrationRunner = CompletableFuture.supplyAsync(() -> {
       try {
-        migrator.run(client);
+        migrator.run(client, writeToTmp);
         return "";
       } catch (Exception ex) {
         return ex.getMessage();
