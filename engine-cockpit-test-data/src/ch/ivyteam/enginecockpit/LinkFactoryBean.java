7adcbfc73299ef64ac37a9ea2a8f32a681e2ba0a
Reto Weiss <reto.weiss@axonivy.com>
2024-06-10 15:57:46 +0200

    XIVY-14293 Add WebTestCockpitLinkFactory
    The view link-factory.xhtml and the LinkFactoryBean gets all static
    public methods of class CockpitLinkFactory from core and creates a link
    to the returned WebLink
    
    The WebTestCockpitLinkFactory then iterates over the links and clicks
    them. Afterwards it tests if the page exists and has the correct title.

---
+++ engine-cockpit-test-data/src/ch/ivyteam/enginecockpit/LinkFactoryBean.java
@@ -0,0 +1,77 @@
+package ch.ivyteam.enginecockpit;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import ch.ivyteam.ivy.engine.cockpit.CockpitLinkFactory;
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.model.value.WebLink;
+import javassist.Modifier;
+
+@ManagedBean
+@RequestScoped
+public class LinkFactoryBean {
+
+  public List<Link> getLinks() {
+    var links = new ArrayList<Link>();
+    links.addAll(Stream.of(CockpitLinkFactory.class.getMethods())
+            .filter(m -> Modifier.isPublic(m.getModifiers()) && Modifier.isStatic(m.getModifiers()))
+            .filter(m -> m.getParameterCount() == 0)
+            .filter(m -> m.getReturnType().equals(WebLink.class))
+            .map(this::toLink)
+            .toList());
+    return links;
+  }
+
+  private Link toLink(Method method) {
+    try {
+      var link = (WebLink) method.invoke(null);
+      return toLink(method.getName(), link);
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  private Link toLink(String title, WebLink link) {
+    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+    var appContext = externalContext.getRequestContextPath();
+    Ivy.log().warn("App context " + appContext);
+    String path;
+    if (!appContext.equals("/designer")) {
+      path = "/system/engine-cockpit/faces/" + link.getRelative();
+    } else {
+      path = appContext + externalContext.getRequestServletPath() + externalContext.getRequestPathInfo();
+      path = path.replace("engine-cockpit-test-data", "engine-cockpit").replace("link-factory.xhtml",
+              link.getRelative());
+    }
+    Ivy.log().warn("Link "+link);
+    link = WebLink.of(path);
+    return new Link(title, link.getAbsolute());
+  }
+
+  public static class Link {
+
+    private String title;
+    private String href;
+
+    public Link(String title, String href) {
+      this.title = title;
+      this.href = href;
+    }
+
+    public String getTitle() {
+      return title;
+    }
+
+    public String getHref() {
+      return href;
+    }
+  }
+}
