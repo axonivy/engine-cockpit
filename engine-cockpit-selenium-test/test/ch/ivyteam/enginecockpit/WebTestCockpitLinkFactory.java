49e03673c557536ec1bfee04026ea9dd1614d28b
Reto Weiss <reto.weiss@axonivy.com>
2024-10-22 10:14:54 +0200

    XIVY-14846 Add CockpitLinkFactory tests for newly added methods
    Add tests for externalDatbase and restClient method on the
    CockpitLinkFactory.
    
    Create a test (dynamically) for each method/link that is tested instead
    of only one test that tests all methods.

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestCockpitLinkFactory.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestCockpitLinkFactory.java
@@ -8,17 +8,23 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.webdriver;
 import static com.codeborne.selenide.WebDriverConditions.title;
 import static com.codeborne.selenide.WebDriverConditions.urlContaining;
+import static org.assertj.core.api.Assertions.fail;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.openqa.selenium.WebDriver;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.codeborne.selenide.CheckResult;
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ObjectCondition;
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 
 import ch.ivyteam.enginecockpit.util.EngineCockpitUtil;
 
@@ -29,26 +35,50 @@ public class WebTestCockpitLinkFactory {
   void beforeEach() {
     login();
   }
+  
+  @TestFactory
+  Collection<DynamicTest> link() {
+    open(EngineCockpitUtil.testViewUrl("link-factory.xhtml"));
+    $("h1").shouldBe(text("CockpitLinkFactory"));
+    var links = $$(".ui-link");
+    var tests = new ArrayList<DynamicTest>();
+    for (var link : links.asDynamicIterable()) {
+      var url = link.attr("href");
+      var name = link.text();
+      tests.add(DynamicTest.dynamicTest(name, () -> link(url))); 
+    }
+    return tests;
+  }
+    
+  private void link(String url) {
+    var link = linkFor(url);
+    link.click();
+    webdriver().shouldHave(urlContaining(url));
+    webdriver().shouldNotHave(title("Not Found"));
+    webdriver().shouldHave(titles("Axon Ivy Engine", "Axon Ivy Cockpit", "Engine Cockpit"));
+  }
+
+  private SelenideElement linkFor(String url) {
+    open(EngineCockpitUtil.testViewUrl("link-factory.xhtml"));
+    $("h1").shouldBe(text("CockpitLinkFactory"));
+    var links = $$(".ui-link");
+    for (var link : links.asDynamicIterable()) {
+      if (link.attr("href").equals(url)) {
+        return link;
+      }
+    }
+    fail("No link found for url " + url);
+    return null;
+  }
 
   @Test
   void links() {
     open(EngineCockpitUtil.testViewUrl("link-factory.xhtml"));
     $("h1").shouldBe(text("CockpitLinkFactory"));
     var links = $$(".ui-link");
-    int pos = 0;
-    while (pos < links.size()) {
-      var link = links.get(pos++);
-      var url = link.attr("href");
-      link.click();
-      webdriver().shouldHave(urlContaining(url));
-      webdriver().shouldNotHave(title("Not Found"));
-      webdriver().shouldHave(titles("Axon Ivy Engine", "Axon Ivy Cockpit", "Engine Cockpit"));
-      Selenide.back();
-      $("h1").shouldBe(text("CockpitLinkFactory"));
-      links = $$(".ui-link");
-    }
+    links.shouldHave(CollectionCondition.sizeGreaterThanOrEqual(7));
   }
-
+  
   private ObjectCondition<WebDriver> titles(String... titles) {
     return new Titles(titles);
   }
