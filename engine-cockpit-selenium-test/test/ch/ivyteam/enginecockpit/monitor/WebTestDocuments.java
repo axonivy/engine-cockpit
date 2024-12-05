7d31838dd11c253c3b9cb7748de1c183e6b5af34
Alexander Suter <alexander.suter@axonivy.com>
2024-12-05 11:23:46 +0100

    Fix SelendIDE deprecation warnings

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestDocuments.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestDocuments.java
@@ -15,8 +15,10 @@ import org.openqa.selenium.By;
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DownloadOptions;
 import com.codeborne.selenide.FileDownloadMode;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.files.FileFilters;
 
 import ch.ivyteam.enginecockpit.util.EngineCockpitUtil;
 import ch.ivyteam.enginecockpit.util.Navigation;
@@ -60,7 +62,8 @@ class WebTestDocuments {
   @Test
   void download() {
     var blobs = table();
-    var download = blobs.tableEntry(1, 9).download(TIMEOUT);
+    var options = DownloadOptions.file().withTimeout(TIMEOUT);
+    var download = blobs.tableEntry(1, 9).download(options);
     assertThat(download.getName()).isEqualTo("louis.txt");
     assertThat(download).content().isEqualTo("louis schreibt man mit s");
   }
