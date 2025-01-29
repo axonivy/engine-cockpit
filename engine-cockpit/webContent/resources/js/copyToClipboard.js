82cc204d8181fe7076aadf8fa5066fa29e5e4769
Lukas Lieb <lukas.lieb@axonivy.com>
2025-01-29 16:02:53 +0100

    Improve copy to clipboard function
    - Do not show alert dialog if wirte to clipboard api was successful.
    Otherwise customers think they need to copy it from there. And
    somethimes the alert dialog also cuts content.
    - Show check icon if copy to clipboard was successful

--- engine-cockpit/webContent/resources/js/copyToClipboard.js
+++ engine-cockpit/webContent/resources/js/copyToClipboard.js
@@ -1,6 +1,16 @@
-function copyToClipboard(content) {
-  if (navigator.clipboard) {
+function copyToClipboard(content, button) {
+  try {
     navigator.clipboard.writeText(content);
+    if (button) {
+      const icon = $(button).find('.ui-icon');
+      icon.removeClass('si-copy-paste')
+      icon.addClass('si-check-1');
+      setTimeout(() => {
+        icon.removeClass('si-check-1');
+        icon.addClass('si-copy-paste')
+      }, 1000);
+    }
+  } catch {
+    alert(content);
   }
-  alert(content);
 }
