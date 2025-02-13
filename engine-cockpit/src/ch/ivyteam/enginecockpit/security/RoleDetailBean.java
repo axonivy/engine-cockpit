3ce5c912164d3c2a90d09abb371bf8336d2bbe9c
Lukas Lieb <lukas.lieb@axonivy.com>
2025-02-13 16:23:28 +0100

    XIVY-15855 Add direct role match to the role member autocomplete

--- engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/security/RoleDetailBean.java
@@ -351,9 +351,18 @@ public class RoleDetailBean {
   }
 
   public List<Role> searchMember(String query) {
-    return roleDataModel.getList().stream()
-        .filter(m -> StringUtils.containsIgnoreCase(m.getName(), query) && !isRoleMemberOfRole(m.getName()))
+    var directMatch = roleDataModel.getList().stream()
+        .filter(m -> StringUtils.equalsIgnoreCase(m.getName(), query))
+        .filter(m -> !isRoleMemberOfRole(m.getName()))
+        .findAny();
+    var result = roleDataModel.getList().stream()
+        .filter(m -> StringUtils.containsIgnoreCase(m.getName(), query))
+        .filter(m -> !isRoleMemberOfRole(m.getName()))
         .limit(10).collect(Collectors.toList());
+    if (directMatch.isPresent() && !result.contains(directMatch.get())) {
+      result.addFirst(directMatch.get());
+    }
+    return result;
   }
 
   public MemberProperty getMemberProperty() {
