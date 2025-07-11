1387be40680949795b567c791885f3388160beed
Jacek <jacek.lajdecki@axonivy.com>
2025-07-11 14:20:10 +0200

    XIVY-17174 Add web service processes test

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/services/WebTestWebServiceProcesses.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/services/WebTestWebServiceProcesses.java
@@ -1,11 +1,17 @@
 package ch.ivyteam.enginecockpit.services;
 
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
+import com.axonivy.ivy.webtest.primeui.PrimeUi;
 
 import ch.ivyteam.enginecockpit.util.Navigation;
 import ch.ivyteam.enginecockpit.util.Tab;
@@ -14,6 +20,7 @@ import ch.ivyteam.enginecockpit.util.Tab;
 class WebTestWebServiceProcesses {
 
   private static final String APP = "test";
+  private static final String SERVICE_NAME = "TestWebServiceProcessCockpit";
 
   @BeforeEach
   void beforeEach() {
@@ -25,5 +32,11 @@ class WebTestWebServiceProcesses {
   @Test
   void webServices() {
     Tab.APP.switchToDefault();
+    $("h2").shouldHave(text("Web Service Processes"));
+
+    $$(Tab.APP.activePanelCss + " .webServiceProcessesTable tr a").shouldHave(size(1));
+
+    var table = PrimeUi.table(By.cssSelector(Tab.APP.activePanelCss + " .webServiceProcessesTable"));
+    table.contains(SERVICE_NAME);
   }
 }
