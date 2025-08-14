8ceee5e0f32bb85ba14b37ca5a9392b7ca603d7e
phhung-axonivy <hung.pham@axonactive.com>
2025-08-14 14:23:52 +0700

    Fix Test

--- engine-cockpit-test/src_test/ch/ivyteam/enginecockpit/services/rest/TestRestTestRunner.java
+++ engine-cockpit-test/src_test/ch/ivyteam/enginecockpit/services/rest/TestRestTestRunner.java
@@ -1,6 +1,7 @@
 package ch.ivyteam.enginecockpit.services.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.apache.commons.lang.StringUtils.EMPTY;
 
 import javax.ws.rs.core.Response.Status;
 
@@ -12,31 +13,31 @@ class TestRestTestRunner {
 
   @Test
   void resultOk() {
-    ConnectionTestResult result = RestTestRunner.toTestResult(Status.OK);
+    ConnectionTestResult result = RestTestRunner.toTestResult(Status.OK, EMPTY, EMPTY, EMPTY, EMPTY);
     assertThat(result.getTestResult()).isEqualTo("Success");
   }
 
   @Test
   void resultOkRedirected() {
-    ConnectionTestResult result = RestTestRunner.toTestResult(Status.TEMPORARY_REDIRECT);
+    ConnectionTestResult result = RestTestRunner.toTestResult(Status.TEMPORARY_REDIRECT, EMPTY, EMPTY, EMPTY, EMPTY);
     assertThat(result.getTestResult()).isEqualTo("Success");
   }
 
   @Test
   void resultError() {
-    ConnectionTestResult result = RestTestRunner.toTestResult(Status.INTERNAL_SERVER_ERROR);
+    ConnectionTestResult result = RestTestRunner.toTestResult(Status.INTERNAL_SERVER_ERROR, EMPTY, EMPTY, EMPTY, EMPTY);
     assertThat(result.getTestResult()).isEqualTo("Error");
   }
 
   @Test
   void resultAuthenificationError() {
-    ConnectionTestResult result = RestTestRunner.toTestResult(Status.UNAUTHORIZED);
+    ConnectionTestResult result = RestTestRunner.toTestResult(Status.UNAUTHORIZED, EMPTY, EMPTY, EMPTY, EMPTY);
     assertThat(result.getTestResult()).isEqualTo("Warning");
   }
 
   @Test
   void resultBadRequest() {
-    ConnectionTestResult result = RestTestRunner.toTestResult(Status.BAD_REQUEST);
+    ConnectionTestResult result = RestTestRunner.toTestResult(Status.BAD_REQUEST, EMPTY, EMPTY, EMPTY, EMPTY);
     assertThat(result.getTestResult()).isEqualTo("Warning");
   }
 }
