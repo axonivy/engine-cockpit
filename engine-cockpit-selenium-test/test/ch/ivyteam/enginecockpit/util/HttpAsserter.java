7b09e7cda40ef2a9512e8d0281cfb8a6a20d0483
Lukas Lieb <lukas.lieb@axonivy.com>
2025-10-31 15:12:07 +0100

    XIVY-17722 Run crawl test also with query params

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/util/HttpAsserter.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/util/HttpAsserter.java
@@ -3,7 +3,6 @@ package ch.ivyteam.enginecockpit.util;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,55 +12,65 @@ import org.jsoup.Jsoup;
 
 public class HttpAsserter {
 
-  public static HttpAssert assertThat(String url) {
-    return new HttpAssert(url);
+  public static HttpAssert assertThat(String url, String sessionId) {
+    return new HttpAssert(url, sessionId);
   }
 
   public static class HttpAssert {
 
-    private final String url;
+    private final String testUrl;
+    private final Set<String> crawled = new HashSet<>();
+    private final Set<String> checked = new HashSet<>();
+    private final int maxDepth = 10;
+    private final String sessionId;
 
-    private HttpAssert(String url) {
-      this.url = url;
+    private HttpAssert(String testUrl, String sessionId) {
+      this.testUrl = testUrl;
+      this.sessionId = sessionId;
     }
 
-    public void hasNoDeadLinks(int maxDepth, String sessionId) {
-      hasNoDeadLinks(maxDepth, 0, new HashSet<>(), new HashSet<>(), sessionId);
+    public void hasNoDeadLinks() {
+      crawled.clear();
+      checked.clear();
+      hasNoDeadLinks(0);
     }
 
-    private void hasNoDeadLinks(int maxDepth, int currentDepth, Set<String> crawled, Set<String> checked, String sessionId) {
+    private void hasNoDeadLinks(int currentDepth) {
+      hasNoDeadLinksForUrl(testUrl, currentDepth);
+    }
+
+    private void hasNoDeadLinksForUrl(String url, int currentDepth) {
       if (maxDepth <= currentDepth) {
         return;
       }
-      if (containsQueryParamIgnore(crawled, url)) {
+      if (crawled.contains(url)) {
         return;
       }
 
-      var linksFound = parseLinks(url, sessionId);
-      System.out.println(url + " found " + linksFound.size() + " links");
+      var linksFound = parseLinks(url);
       crawled.add(url);
 
-      var deadLinks = findDeadLinks(linksFound, checked, sessionId);
+      var deadLinks = findDeadLinks(linksFound);
       Assertions.assertThat(deadLinks).as("found dead links on " + url).isEmpty();
       checked.addAll(linksFound);
 
       currentDepth += 1;
       for (var link : linksFound) {
-        if (link.contains("faces/view/engine-cockpit")) {
-          assertThat(link).hasNoDeadLinks(maxDepth, currentDepth, crawled, checked, sessionId);
+        if (link.contains("/faces/") && link.contains("/engine-cockpit/")) {
+          hasNoDeadLinksForUrl(link, currentDepth);
         }
       }
     }
 
-    private Set<String> findDeadLinks(Set<String> linksToCheck, Set<String> linksAlreadyChecked, String sessionId) {
+    private Set<String> findDeadLinks(Set<String> linksToCheck) {
       return linksToCheck.stream()
-          .filter(link -> !containsQueryParamIgnore(linksAlreadyChecked, link))
-          .filter(link -> !check(link, sessionId))
+          .filter(link -> !checked.contains(link))
+          .filter(link -> !check(link))
           .collect(Collectors.toSet());
     }
 
-    private static Set<String> parseLinks(String url, String sessionId) {
-      System.out.println(url + " crawl");
+    private Set<String> parseLinks(String url) {
+      System.out.println("crawl " + url);
       var result = new HashSet<String>();
       try {
         var con = Jsoup.connect(url);
@@ -74,31 +83,25 @@ public class HttpAsserter {
           if (StringUtils.isEmpty(href)) {
             continue;
           }
-          var u = URI.create(href);
+          var u = URI.create(href.replace(' ', '+'));
           if (u.isAbsolute()) {
             result.add(href);
           } else {
             var absolute = URI.create(url).resolve(u).toString();
-            if (!containsQueryParamIgnore(result, absolute)) {
-              result.add(absolute);
-            }
+            result.add(absolute);
           }
         }
+        System.out.println("Found " + result.size() + " links");
         return result;
       } catch (IOException ex) {
         throw new RuntimeException("Could not crawl " + url, ex);
       }
     }
 
-    private static boolean containsQueryParamIgnore(Set<String> crawled, String url) {
-      var urlWithoutQuery = StringUtils.substringBefore(url, "?");
-      return crawled.stream().anyMatch(c -> Objects.equals(StringUtils.substringBefore(c, "?"), urlWithoutQuery));
-    }
-
-    private boolean check(String urlToCheck, String sessionId) {
-      System.out.println("check " + urlToCheck);
+    private boolean check(String url) {
+      System.out.println("check " + url);
       try {
-        var con = Jsoup.connect(urlToCheck);
+        var con = Jsoup.connect(url);
         con.cookie("JSESSIONID", sessionId);
         con.get();
         return true;
