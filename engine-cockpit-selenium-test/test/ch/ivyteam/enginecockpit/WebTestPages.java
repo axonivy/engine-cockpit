5a09aea60355f264a77773820162616f125e53ad
Lukas Lieb <lukas.lieb@axonivy.com>
2025-11-10 07:57:54 +0100

    Stabilize dead links page test

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestPages.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/WebTestPages.java
@@ -27,7 +27,11 @@ import ch.ivyteam.enginecockpit.util.HttpAsserter;
 @IvyWebTest
 class WebTestPages {
 
-  private static List<String> IGNORE_PAGES = List.of(EngineUrl.create().app("test").path("ws/engine-cockpit-test-data/197F8BA7DD34B7A2").queryParam("WSDL", "").toUrl());
+  private static List<String> IGNORE_PAGES = List.of(
+      EngineUrl.create().app("test").path("ws/engine-cockpit-test-data/197F8BA7DD34B7A2").queryParam("WSDL", "").toUrl(),
+      EngineUrl.create().app("test-ad").path("ws/engine-cockpit-test-data/197F8BA7DD34B7A2").queryParam("WSDL", "").toUrl(),
+      EngineUrl.create().app("demo-portal").path("ws/engine-cockpit-test-data/197F8BA7DD34B7A2").queryParam("WSDL", "").toUrl()
+  );
   private Path webContentDir;
 
   @BeforeEach
