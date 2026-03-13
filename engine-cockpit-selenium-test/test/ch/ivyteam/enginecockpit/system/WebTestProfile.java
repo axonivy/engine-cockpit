bfe4ae2ac7693a274f9e3c14628c200dbbbcaf89
ivy-dar <dominik.arnold@axonivy.com>
2026-03-13 14:39:05 +0100

    Add happypath test for profile

---
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/system/WebTestProfile.java
@@ -0,0 +1,48 @@
+package ch.ivyteam.enginecockpit.system;
+
+import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
+import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.open;
+import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.viewUrl;
+import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.getAdminUser;
+import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.assertCurrentUrlContains;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.text;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.axonivy.ivy.webtest.IvyWebTest;
+
+@IvyWebTest
+public class WebTestProfile {
+  
+  private final String PROFILE = "profile.xhtml";
+  private final String adminUser = getAdminUser();
+
+  private void navigateToProfilePage() {
+    open(viewUrl(PROFILE));
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    login();
+  }
+  
+  @Test
+  void testLinkToProfile() {
+    $("ul.layout-topbar-actions").shouldBe(visible);
+    $("li.topbar-item.user-profile").shouldBe(visible);
+    $("li.topbar-item.user-profile > a").click();
+    $("#profileLink").shouldBe(visible).click();
+    assertCurrentUrlContains(PROFILE);
+  }
+
+  @Test
+  void testProfileUsernameMatch() {
+    navigateToProfilePage();
+    $("#profileForm:userName").shouldBe(visible).shouldHave(text(adminUser));
+  }
+
+}
