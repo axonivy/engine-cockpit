3e2fccaa97021d952501c3c583c2cb8467831de2
ivy-tgr <tobias.gretler@axonivy.com>
2026-05-22 13:08:18 +0200

    XIVY-18948 rewrite configuration ui:include to composite component

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/configuration/WebTestConfiguration.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/configuration/WebTestConfiguration.java
@@ -49,7 +49,26 @@ class WebTestConfiguration {
 
   private static final By CONTENT_FILTER_BTN = By.id("config:form:configTable:filterBtn");
   private static final By TABLE_ID = By.id("config:form:configTable");
+  private static final By APPLICATION_CONTENT_FILTER_BTN = By.id("configuration:config:form:configTable:filterBtn");
+  private static final By APPLICATION_TABLE_ID = By.id("configuration:config:form:configTable");
+  private static final SelectorContext SYSTEM_CONTEXT = new SelectorContext(CONTENT_FILTER_BTN, TABLE_ID, "config", "contentFilter");
+  private static final SelectorContext APPLICATION_CONTEXT =
+      new SelectorContext(APPLICATION_CONTENT_FILTER_BTN, APPLICATION_TABLE_ID, "configuration:config", "configuration:contentFilter");
   private Table table;
+
+  private static final class SelectorContext {
+    private final By contentFilterBtn;
+    private final By tableId;
+    private final String configPrefix;
+    private final String contentFilterPrefix;
+
+    private SelectorContext(By contentFilterBtn, By tableId, String configPrefix, String contentFilterPrefix) {
+      this.contentFilterBtn = contentFilterBtn;
+      this.tableId = tableId;
+      this.configPrefix = configPrefix;
+      this.contentFilterPrefix = contentFilterPrefix;
+    }
+  }
 
   @BeforeEach
   void beforeEach() {
@@ -78,7 +97,7 @@ class WebTestConfiguration {
     @BeforeEach
     void beforeEach() {
       Navigation.toSystemConfig();
-      table = new Table(TABLE_ID, "span");
+      table = new Table(SYSTEM_CONTEXT.tableId, "span");
     }
 
     @Test
@@ -94,32 +113,32 @@ class WebTestConfiguration {
     @Test
     void hideDefaults() {
       var config = "Data.AppDirectory";
-      assertContentFilterText("Filter: none");
+      assertContentFilterText(SYSTEM_CONTEXT, "Filter: none");
       table.firstColumnShouldBe(itemWithText(config));
-      toggleDefaultFilter();
-      assertContentFilterText("Filter: defined");
+      toggleDefaultFilter(SYSTEM_CONTEXT);
+      assertContentFilterText(SYSTEM_CONTEXT, "Filter: defined");
       table.firstColumnShouldBe(noneMatch("Config not visible", e -> config.equals(e.getText())));
 
-      resetContentFilter();
+      resetContentFilter(SYSTEM_CONTEXT);
       table.firstColumnShouldBe(itemWithText(config));
     }
 
     @Test
     void showSecuritySystemConfigs() {
       var config = "SecuritySystems.test-ad.IdentityProvider.Name";
-      assertContentFilterText("Filter: none");
+      assertContentFilterText(SYSTEM_CONTEXT, "Filter: none");
       table.firstColumnShouldBe(noneMatch("Config not visible", e -> config.equals(e.getText())));
-      toggleFilter(List.of("Show Security Systems"));
-      assertContentFilterText("Filter: Security Systems");
+      toggleFilter(SYSTEM_CONTEXT, List.of("Show Security Systems"));
+      assertContentFilterText(SYSTEM_CONTEXT, "Filter: Security Systems");
       table.firstColumnShouldBe(itemWithText(config));
-      resetContentFilter();
+      resetContentFilter(SYSTEM_CONTEXT);
       table.firstColumnShouldBe(noneMatch("Config not visible", e -> config.equals(e.getText())));
     }
 
     @Test
     void showConfigFile() {
       String key = "Connector.HTTP.AllowTrace";
-      assertShowConfigFile(key);
+      assertShowConfigFile(SYSTEM_CONTEXT, key);
     }
 
     @Test
@@ -138,12 +157,12 @@ class WebTestConfiguration {
       PrimeUi.selectOne(By.id("config:editConfigurationForm:editConfigurationValue"))
           .selectItemByLabel("SSL");
       $("#config\\:editConfigurationForm\\:saveEditConfiguration").click();
-      assertAndCloseGrowl(config);
+        assertAndCloseGrowl(SYSTEM_CONTEXT, config);
       table.row(config).shouldNotHave(cssClass("default-value"));
 
-      toggleDefaultFilter();
+        toggleDefaultFilter(SYSTEM_CONTEXT);
       table.firstColumnShouldBe(itemWithText(config));
-      assertResetConfig(config);
+        assertResetConfig(SYSTEM_CONTEXT, config);
       table.firstColumnShouldBe(noneMatch("Config no longer listed under defined values",
           e -> e.getText().equals(config)));
     }
@@ -152,27 +171,27 @@ class WebTestConfiguration {
     void editConfig_booleanFormat() {
       var config = "EMail.Server.SSL.UseKey";
       table.clickButtonForEntry(config, "editConfigBtn");
-      assertThatConfigEditModalIsVisible(config, "false", "Specifies whetere a client certificate should be used for authentication");
+      assertThatConfigEditModalIsVisible(SYSTEM_CONTEXT, config, "false", "Specifies whetere a client certificate should be used for authentication");
     }
 
     @Test
     void editConfig_numberFormat() {
       var config = "SystemDb.MaxConnections";
       table.clickButtonForEntry(config, "editConfigBtn");
-      assertThatConfigEditModalIsVisible(config, "50", "Maximum number");
+      assertThatConfigEditModalIsVisible(SYSTEM_CONTEXT, config, "50", "Maximum number");
     }
 
     @Test
     void editConfig_enumerationFormat() {
       var config = "SystemTask.Failure.Behaviour";
       table.clickButtonForEntry(config, "editConfigBtn");
-      assertThatConfigEditModalIsVisible(config, "FAIL_TASK_DO_RETRY", "Defines the behaviour");
+      assertThatConfigEditModalIsVisible(SYSTEM_CONTEXT, config, "FAIL_TASK_DO_RETRY", "Defines the behaviour");
     }
 
     @Test
     void restartHint() {
       var config = "Connector.HTTP.Address";
-      assertEditConfig(config, "", "hi", "For servers with more than one IP address");
+      assertEditConfig(SYSTEM_CONTEXT, config, "", "hi", "For servers with more than one IP address");
       Selenide.sleep(100); // restart will be evaluated by async file watcher
       refresh();
       table.valueForEntryShould(config, 2, exactText("hi"));
@@ -186,7 +205,7 @@ class WebTestConfiguration {
             return health.attr("class").contains("active-topmenuitem");
           });
       health.$$("li").shouldHave(anyMatch("message contains", e -> e.getText().contains("Restart is required")));
-      assertResetConfig(config);
+      assertResetConfig(SYSTEM_CONTEXT, config);
     }
   }
 
@@ -200,8 +219,8 @@ class WebTestConfiguration {
 
     private void navigateTo(String app) {
       Navigation.toApplicationDetail(app);
-      $(CONTENT_FILTER_BTN).scrollIntoView(ScrollIntoViewOptions.instant().block(Block.center).inline(Inline.center));
-      table = new Table(TABLE_ID, "span");
+      $(APPLICATION_CONTEXT.contentFilterBtn).scrollIntoView(ScrollIntoViewOptions.instant().block(Block.center).inline(Inline.center));
+      table = new Table(APPLICATION_CONTEXT.tableId, "span");
     }
 
     @Test
@@ -213,9 +232,9 @@ class WebTestConfiguration {
     void hideDefaults() {
       var config = "Data.FilesDirectory";
       table.firstColumnShouldBe(itemWithText(config));
-      toggleDefaultFilter();
+      toggleDefaultFilter(APPLICATION_CONTEXT);
       table.firstColumnShouldBe(noneMatch("Hide default values", e -> Objects.equals(e.getText(), config)));
-      resetContentFilter();
+      resetContentFilter(APPLICATION_CONTEXT);
       table.firstColumnShouldBe(itemWithText(config));
     }
 
@@ -242,19 +261,19 @@ class WebTestConfiguration {
     private void assertShowAppConfigFilter(String filter, String config) {
       table.firstColumnShouldBe(noneMatch("Config should not be listed in the config table per default: " + config,
           element -> Objects.equals(element.getText(), config)));
-      toggleFilter(List.of(filter));
+      toggleFilter(APPLICATION_CONTEXT, List.of(filter));
       table.firstColumnShouldBe(itemWithText(config));
-      resetContentFilter();
+      resetContentFilter(APPLICATION_CONTEXT);
       table.firstColumnShouldBe(noneMatch("Config should not be listed in the config table after reset filter: " + config,
           element -> Objects.equals(element.getText(), config)));
     }
 
     @Test
     void dynamicExpressions() {
-      toggleFilter(List.of("Show Rest Clients"));
+      toggleFilter(APPLICATION_CONTEXT, List.of("Show Rest Clients"));
       var dynamic = "RestClients.second-rest.Properties.appKey";
       table.clickButtonForEntry(dynamic, "editConfigBtn");
-      new ConfigAssert("config")
+      new ConfigAssert(APPLICATION_CONTEXT.configPrefix)
           .assertDefault("${ivy.var.password}")
           .assertValue("${ivy.var.password}");
     }
@@ -263,15 +282,15 @@ class WebTestConfiguration {
     void showConfigFile() {
       navigateTo("demo-portal");
       var key = "OverrideProject";
-      assertShowConfigFile(key);
+      assertShowConfigFile(APPLICATION_CONTEXT, key);
     }
 
     @Test
     void overrideProject_pmvSelector() {
       var config = "OverrideProject";
       table.clickButtonForEntry(config, "editConfigBtn");
-      assertThatConfigEditModalIsVisible(config, " ", "Defines a project containing overriding SubProcesses", "");
-      $(By.id("config:editConfigurationForm:editConfigurationValue"))
+      assertThatConfigEditModalIsVisible(APPLICATION_CONTEXT, config, " ", "Defines a project containing overriding SubProcesses", "");
+      $(By.id(APPLICATION_CONTEXT.configPrefix + ":editConfigurationForm:editConfigurationValue"))
           .shouldHave(cssClass("ui-selectonemenu"));
     }
   }
@@ -282,16 +301,16 @@ class WebTestConfiguration {
     @BeforeEach
     void beforeEach() {
       Navigation.toApplicationDetail("demo-portal");
-      $(By.id("config:form:configTable:filterBtn")).scrollIntoView(ScrollIntoViewOptions.instant().block(Block.center).inline(Inline.center));
-      table = new Table(TABLE_ID, "span");
+      $(APPLICATION_CONTEXT.contentFilterBtn).scrollIntoView(ScrollIntoViewOptions.instant().block(Block.center).inline(Inline.center));
+      table = new Table(APPLICATION_CONTEXT.tableId, "span");
     }
 
     @Test
     void defaultPages() {
       var config = "StandardProcess.DefaultPages";
       table.clickButtonForEntry(config, "editConfigBtn");
-      assertThatConfigEditModalIsVisible(config, "com.axonivy.portal:portal", "default-pages", "auto");
-      $(By.id("config:editConfigurationForm:editConfigurationValue")).shouldHave(cssClass("ui-selectonemenu"));
+      assertThatConfigEditModalIsVisible(APPLICATION_CONTEXT, config, "com.axonivy.portal:portal", "default-pages", "auto");
+      $(By.id(APPLICATION_CONTEXT.configPrefix + ":editConfigurationForm:editConfigurationValue")).shouldHave(cssClass("ui-selectonemenu"));
     }
   }
 
@@ -303,57 +322,58 @@ class WebTestConfiguration {
     table.firstColumnShouldBe(exactTexts(search));
   }
 
-  private void toggleDefaultFilter() {
+  private void toggleDefaultFilter(SelectorContext context) {
     table.body().find(".default-value").shouldBe(visible);
-    toggleFilter(List.of("Show only defined values"));
+    toggleFilter(context, List.of("Show only defined values"));
     table.body().find(".default-value").shouldNotBe(visible);
   }
 
-  private void toggleFilter(List<String> filter) {
-    $(CONTENT_FILTER_BTN).shouldBe(visible).click();
-    $$("#contentFilter\\:filterPanel .ui-chkbox").shouldBe(sizeGreaterThanOrEqual(1));
-    var checkboxes = PrimeUi.selectManyCheckbox(By.cssSelector("#contentFilter\\:filterForm\\:filterCheckboxes"));
+  private void toggleFilter(SelectorContext context, List<String> filter) {
+    $(context.contentFilterBtn).shouldBe(visible).click();
+    String escapedContentFilterPrefix = context.contentFilterPrefix.replace(":", "\\:");
+    $$("#" + escapedContentFilterPrefix + "\\:filterPanel .ui-chkbox").shouldBe(sizeGreaterThanOrEqual(1));
+    var checkboxes = PrimeUi.selectManyCheckbox(By.cssSelector("#" + escapedContentFilterPrefix + "\\:filterForm\\:filterCheckboxes"));
     checkboxes.setCheckboxes(filter);
-    $("#contentFilter\\:filterForm\\:applyFilter").shouldBe(visible).click();
+    $("#" + escapedContentFilterPrefix + "\\:filterForm\\:applyFilter").shouldBe(visible).click();
   }
 
-  private void assertShowConfigFile(String key) {
+  private void assertShowConfigFile(SelectorContext context, String key) {
     table.clickButtonForEntry(key, "tasksButton");
     table.buttonMenuForEntryShouldBeVisible(key, "activityMenu");
     table.clickButtonForEntry(key, "showFileBtn");
-    $(By.id("config:showConfigurationFileModal")).shouldBe(visible);
-    $(By.id("config:showConfigurationFileForm:codeBlock")).shouldHave(value(key));
+    $(By.id(context.configPrefix + ":showConfigurationFileModal")).shouldBe(visible);
+    $(By.id(context.configPrefix + ":showConfigurationFileForm:codeBlock")).shouldHave(value(key));
   }
 
-  private void assertResetConfig(String key) {
+  private void assertResetConfig(SelectorContext context, String key) {
     table.clickButtonForEntry(key, "tasksButton");
     table.buttonMenuForEntryShouldBeVisible(key, "activityMenu");
     table.clickButtonForEntry(key, "resetConfigBtn");
-    $("#config\\:resetConfigConfirmDialog").shouldBe(visible);
+    $(By.id(context.configPrefix + ":resetConfigConfirmDialog")).shouldBe(visible);
 
-    $("#config\\:resetConfigConfirmForm\\:resetConfigConfirmYesBtn").click();
-    $("#config\\:form\\:msgs_container").shouldHave(text(key), text("reset"));
+    $(By.id(context.configPrefix + ":resetConfigConfirmForm:resetConfigConfirmYesBtn")).click();
+    $(By.id(context.configPrefix + ":form:msgs_container")).shouldHave(text(key), text("reset"));
   }
 
-  private void assertEditConfig(String key, String value, String newValue, String desc) {
+  private void assertEditConfig(SelectorContext context, String key, String value, String newValue, String desc) {
     table.clickButtonForEntry(key, "editConfigBtn");
-    assertThatConfigEditModalIsVisible(key, value, desc, "");
+    assertThatConfigEditModalIsVisible(context, key, value, desc, "");
 
-    $("#config\\:editConfigurationForm\\:editConfigurationValue").clear();
-    $("#config\\:editConfigurationForm\\:editConfigurationValue").sendKeys(newValue);
-    $("#config\\:editConfigurationForm\\:saveEditConfiguration").click();
-    assertAndCloseGrowl(key);
+    $(By.id(context.configPrefix + ":editConfigurationForm:editConfigurationValue")).clear();
+    $(By.id(context.configPrefix + ":editConfigurationForm:editConfigurationValue")).sendKeys(newValue);
+    $(By.id(context.configPrefix + ":editConfigurationForm:saveEditConfiguration")).click();
+    assertAndCloseGrowl(context, key);
     table.valueForEntryShould(key, 2, exactText(newValue));
-    $("#config\\:form\\:msgs_container").shouldNotBe(visible);
+    $(By.id(context.configPrefix + ":form:msgs_container")).shouldNotBe(visible);
   }
 
-  private void assertThatConfigEditModalIsVisible(String key, String value, String desc) {
-    assertThatConfigEditModalIsVisible("config", key, value, desc, value);
+  private void assertThatConfigEditModalIsVisible(SelectorContext context, String key, String value, String desc) {
+    assertThatConfigEditModalIsVisible(context.configPrefix, key, value, desc, value);
   }
 
-  private void assertThatConfigEditModalIsVisible(String key, String value, String desc,
+  private void assertThatConfigEditModalIsVisible(SelectorContext context, String key, String value, String desc,
       String defaultValue) {
-    assertThatConfigEditModalIsVisible("config", key, value, desc, defaultValue);
+    assertThatConfigEditModalIsVisible(context.configPrefix, key, value, desc, defaultValue);
   }
 
   public static void assertThatConfigEditModalIsVisible(String idPrefix, String key, String value,
@@ -365,18 +385,19 @@ class WebTestConfiguration {
         .assertValue(value);
   }
 
-  private void resetContentFilter() {
-    $(CONTENT_FILTER_BTN).shouldBe(visible).click();
-    $("#contentFilter\\:filterForm\\:resetFilterBtn").shouldBe(visible).click();
-    assertContentFilterText("Filter: none");
+  private void resetContentFilter(SelectorContext context) {
+    $(context.contentFilterBtn).shouldBe(visible).click();
+    String escapedContentFilterPrefix = context.contentFilterPrefix.replace(":", "\\:");
+    $("#" + escapedContentFilterPrefix + "\\:filterForm\\:resetFilterBtn").shouldBe(visible).click();
+    assertContentFilterText(context, "Filter: none");
   }
 
-  private void assertContentFilterText(String expectedFilter) {
-    $(CONTENT_FILTER_BTN).shouldHave(attribute("title", expectedFilter));
+  private void assertContentFilterText(SelectorContext context, String expectedFilter) {
+    $(context.contentFilterBtn).shouldHave(attribute("title", expectedFilter));
   }
 
-  private void assertAndCloseGrowl(String key) {
-    $("#config\\:form\\:msgs_container").shouldHave(text(key), text("saved"));
+  private void assertAndCloseGrowl(SelectorContext context, String key) {
+    $(By.id(context.configPrefix + ":form:msgs_container")).shouldHave(text(key), text("saved"));
     executeJavaScript("arguments[0].click();", $(".ui-growl-icon-close"));
   }
 
