08683a3edc61536aad4e4975155ea0949203bf9f
Alexander Suter <alexander.suter@axonivy.com>
2026-06-10 10:46:19 +0200

    Remove unused variable

--- engine-cockpit/src/ch/ivyteam/enginecockpit/info/Application.java
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/info/Application.java
@@ -3,11 +3,8 @@ package ch.ivyteam.enginecockpit.info;
 import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.application.ReleaseState;
 import ch.ivyteam.ivy.application.restricted.IApplicationInternal;
-import ch.ivyteam.log.Logger;
 
 public class Application {
-
-  private static final Logger LOGGER = Logger.getLogger(Application.class);
 
   private final IApplicationInternal app;
   private final String name;
