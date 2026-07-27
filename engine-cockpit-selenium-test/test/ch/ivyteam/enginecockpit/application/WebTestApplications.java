cd6a4e24c0860c972f79a0669c676ff41662383e
ivy-tgr <tobias.gretler@axonivy.com>
2026-07-27 11:28:26 +0200

    XIVY-19018 Improved WebTestApplications

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/application/WebTestApplications.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/application/WebTestApplications.java
@@ -2,11 +2,12 @@ package ch.ivyteam.enginecockpit.application;
 
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.assertCurrentUrlContains;
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static org.assertj.core.api.Assertions.assertThat;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,34 +30,40 @@ class WebTestApplications {
   @Test
   void tableHasApplications() {
     var table = applicationsTable();
-    table.rows().shouldHave(sizeGreaterThan(0));
+    table.rows().shouldHave(size(3));
   }
 
   @Test
-  void applicationLinkOpensDetailsWithCorrectName() {
+  void openReleasedVersions() {
     var table = applicationsTable();
-    table.rows().shouldHave(sizeGreaterThan(0));
+    table.rows().shouldHave(size(3));
 
-    var applicationLink = table.tableEntry(1, 1).$("a");
-    assertThat(applicationLink.getText()).isNotBlank();
+    var applicationLink = table.tableEntry(1, 2).$("a");
     applicationLink.shouldBe(visible).click();
 
-    assertCurrentUrlContains("application.xhtml");
-    var applicationName = $("[id$='name']").shouldBe(visible).getText();
-    assertThat(applicationName).isNotBlank();
+    assertCurrentUrlContains("application-version.xhtml");
   }
 
   @Test
-  void newApplicationDialog() {    
+  void newAppDialog_validate() {    
     $(By.id("form:createApplicationBtn")).click();
     $(By.id("newApplication:newApplicationModal")).shouldBe(visible);
-    var secSystemLabel = $("[id$='newApplicationSecSystemInput_label']").getText();
-    assertThat(secSystemLabel).isNotBlank();
-    
     $(By.id("newApplication:newApplicationForm:saveNewApplication")).click();
-    $(By.id("newApplication:newApplicationForm:newApplicationNameMessage")).shouldBe(visible);
+    $(By.id("newApplication:newApplicationForm:newApplicationNameMessage")).shouldBe(visible).shouldHave(text("Value is required"));
     $(By.id("newApplication:newApplicationForm:cancelNewApplication")).click();
     $(By.id("newApplication:newApplicationModal")).should(not(visible));
+  }
+
+  @Test
+  void searchApplication() {
+    applicationsTable().rows().shouldHave(size(3));
+
+    $(By.id("form:tabs:securitySystemTabView:0:applicationsTable:globalFilter")).sendKeys("demo-portal");
+    applicationsTable().rows().shouldHave(size(1));
+
+    $(By.id("form:tabs:securitySystemTabView:0:applicationsTable:globalFilter")).clear();
+    applicationsTable().rows().shouldHave(size(3)); 
+
   }
 
   private Table applicationsTable() {
