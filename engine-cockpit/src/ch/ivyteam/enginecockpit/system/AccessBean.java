64f39e69ead504fc881ac9305cba45017c76a224
jackra1n <45038833+jackra1n@users.noreply.github.com>
2026-08-20 15:46:56 +0200

    XIVY-19442 Hide some functions on PaaS (#2092)
    * XIVY-19442 Hide some functions on PaaS
    
    * XIVY-19442 Add web tests

---
+++ engine-cockpit/src/ch/ivyteam/enginecockpit/system/AccessBean.java
@@ -0,0 +1,25 @@
+package ch.ivyteam.enginecockpit.system;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+import ch.ivyteam.ivy.configuration.restricted.IConfiguration;
+
+@Named
+@ApplicationScoped
+public class AccessBean implements Serializable {
+
+  private static final String PAAS_CONFIG_KEY = "Cockpit.PaaS";
+  private static final Set<String> PAAS_RESTRICTED_AREAS = Set.of("system", "securitySystems", "logs");
+
+  public boolean hasAccess(String area) {
+    return !isPaaS() || !PAAS_RESTRICTED_AREAS.contains(area);
+  }
+
+  protected boolean isPaaS() {
+    return IConfiguration.instance().getOrDefault(PAAS_CONFIG_KEY, boolean.class);
+  }
+}
