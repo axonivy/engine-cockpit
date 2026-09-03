1c473a0956379fbf255c9e0526b408336064f6e0
Jacek <jacek.lajdecki@axonivy.com>
2026-09-03 12:22:29 +0200

    test logging controls and level updates

--- engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestLogging.java
+++ engine-cockpit-selenium-test/test/ch/ivyteam/enginecockpit/monitor/WebTestLogging.java
@@ -4,16 +4,22 @@ import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.login;
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.open;
 import static ch.ivyteam.enginecockpit.util.EngineCockpitUtil.viewUrl;
 import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.cssClass;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import com.axonivy.ivy.webtest.IvyWebTest;
 import com.axonivy.ivy.webtest.primeui.PrimeUi;
+import com.codeborne.selenide.SelenideElement;
 
 import ch.ivyteam.enginecockpit.util.Table;
 
@@ -31,14 +37,26 @@ class WebTestLogging {
     open(viewUrl("logging.xhtml"));
   }
 
+  @AfterEach
+  void afterEach() {
+    var root = rootLoggerRow();
+    var reset = root.findAll(By.tagName("button"))
+        .findBy(attribute("title", "Reset runtime level"));
+    if (reset.exists()) {
+      reset.click();
+    }
+  }
+
   @Test
   void view() {
-    $(CONFIGURATION_INFO).shouldBe(visible);
+    $(CONFIGURATION_INFO).shouldBe(visible)
+        .shouldHave(text("Configuration file"), text("Last loaded at"), text("Configuration last modified"));
 
     var loggerTable = new Table(LOGGER_TABLE);
     loggerTable.headerShouldBe(exactTexts("Name", "Configured level", "Effective level", "Appenders", "Additive", "Set level"));
     loggerTable.rows().shouldHave(sizeGreaterThan(0));
-    $(LOGGER_TABLE).findAll(".log-level").shouldHave(sizeGreaterThan(0));
+    $(LOGGER_TABLE).findAll(By.className("log-level")).shouldHave(sizeGreaterThan(0));
+    $(LOGGER_TABLE).shouldHave(text("root"));
 
     var appenderTable = new Table(APPENDER_TABLE);
     appenderTable.headerShouldBe(exactTexts("Name", "Type", "Target"));
@@ -46,9 +64,43 @@ class WebTestLogging {
   }
 
   @Test
+  void filter() {
+    var loggerTable = new Table(LOGGER_TABLE, "span");
+    loggerTable.search("root");
+    var visibleRows = loggerTable.rows().filter(visible);
+
+    visibleRows.shouldHave(size(1));
+    visibleRows.first().shouldHave(text("root"));
+  }
+
+  @Test
+  void setLevelUpdatesLoggerTable() {
+    var loggerTable = new Table(LOGGER_TABLE, "span");
+    loggerTable.search("root");
+    var root = loggerTable.rows().filter(visible).first();
+    var levelMenu = root.find(By.className("ui-selectonemenu"));
+
+    PrimeUi.selectOne(By.id(levelMenu.getAttribute("id"))).selectItemByLabel("TRACE");
+    root.findAll(By.tagName("button"))
+        .findBy(attribute("title", "Set level")).click();
+
+    $(By.className("ui-growl-message")).shouldHave(text("Logger level changed"),
+        text("Logger 'root' now uses level 'TRACE'."));
+    var effectiveLevel = rootLoggerRow().find(By.xpath("./td[3]"))
+        .find(By.className("log-level"));
+    effectiveLevel.shouldHave(text("TRACE"), cssClass("log-level-TRACE"));
+  }
+
+  @Test
   void showAllLoggers() {
     var checkbox = PrimeUi.selectBooleanCheckbox(SHOW_ALL_LOGGERS);
     checkbox.shouldBeChecked(false).setChecked().shouldBeChecked(true);
     new Table(LOGGER_TABLE).rows().shouldHave(sizeGreaterThan(0));
+  }
+
+  private SelenideElement rootLoggerRow() {
+    var loggerTable = new Table(LOGGER_TABLE, "span");
+    loggerTable.search("root");
+    return loggerTable.rows().filter(visible).first();
   }
 }
